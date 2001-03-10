0fc79f29fd9e1f195c67a0bc61d6eacbf3b759c2
Masatoshi TSUCHIYA <tsuchiya@namazu.org>
2001-03-10 02:48:52 +0000

    * w3m.el (w3m-entity-alist): Eval at compile time.; Add character entity references for ISO 8859-1 characters.

--- w3m.el
+++ w3m.el
@@ -7,7 +7,8 @@
 ;;          Satoru Takabayashi <satoru-t@is.aist-nara.ac.jp>,
 ;;          Hideyuki SHIRAI    <shirai@meadowy.org>,
 ;;          Keisuke Nishida    <kxn30@po.cwru.edu>,
-;;          Yuuichi Teranishi  <teranisi@gohome.org>
+;;          Yuuichi Teranishi  <teranisi@gohome.org>,
+;;          Akihiro Arisawa    <ari@mbf.sphere.ne.jp>
 ;; Keywords: w3m, WWW, hypermedia
 
 ;; w3m.el is free software; you can redistribute it and/or modify it
@@ -568,13 +569,50 @@ See also `w3m-search-engine-alist'."
 (defconst w3m-extended-charcters-table
   '(("\xa0" . " ")))
 
-(defconst w3m-entity-alist		; html character entities and values
-  '(("nbsp" . " ")
-    ("gt" . ">")
-    ("lt" . "<")
-    ("amp" . "&")
-    ("quot" . "\"")
-    ("apos" . "'")))
+(eval-and-compile
+  (defconst w3m-entity-alist	  ; html character entities and values
+    (eval-when-compile
+      (let ((basic-entity-alist
+	     '(("nbsp" . " ")
+	       ("gt" . ">")
+	       ("lt" . "<")
+	       ("amp" . "&")
+	       ("quot" . "\"")
+	       ("apos" . "'")))
+	    (latin1-entity
+	     '(				;("nbsp" . 160)
+	       ("iexcl" . 161) ("cent" . 162) ("pound" . 163)
+	       ("curren" . 164) ("yen" . 165) ("brvbar" . 166) ("sect" . 167)
+	       ("uml" . 168) ("copy" . 169) ("ordf" . 170) ("laquo" . 171)
+	       ("not" . 172)  ("shy" . 173) ("reg" . 174) ("macr" . 175)
+	       ("deg" . 176) ("plusmn" . 177) ("sup2" . 178) ("sup3" . 179)
+	       ("acute" . 180) ("micro" . 181) ("para" . 182) ("middot" . 183)
+	       ("cedil" . 184) ("sup1" . 185) ("ordm" . 186) ("raquo" . 187)
+	       ("frac14" . 188) ("frac12" . 189) ("frac34" . 190) ("iquest" . 191)
+	       ("Agrave" . 192) ("Aacute" . 193) ("Acirc" . 194) ("Atilde" . 195)
+	       ("Auml" . 196) ("Aring" . 197) ("AElig" . 198) ("Ccedil" . 199)
+	       ("Egrave" . 200) ("Eacute" . 201) ("Ecirc" . 202) ("Euml" . 203)
+	       ("Igrave" . 204) ("Iacute" . 205) ("Icirc" . 206) ("Iuml" . 207)
+	       ("ETH"  . 208) ("Ntilde" . 209) ("Ograve" . 210) ("Oacute" . 211)
+	       ("Ocirc" . 212) ("Otilde" . 213) ("Ouml" . 214) ("times" . 215)
+	       ("Oslash" . 216) ("Ugrave" . 217) ("Uacute" . 218) ("Ucirc" . 219)
+	       ("Uuml" . 220) ("Yacute" . 221) ("THORN" . 222) ("szlig" . 223)
+	       ("agrave" . 224) ("aacute" . 225) ("acirc" . 226) ("atilde" . 227)
+	       ("auml" . 228) ("aring" . 229) ("aelig" . 230) ("ccedil" . 231)
+	       ("egrave" . 232) ("eacute" . 233) ("ecirc" . 234) ("euml" . 235)
+	       ("igrave" . 236) ("iacute" . 237) ("icirc" . 238) ("iuml" . 239)
+	       ("eth" . 240) ("ntilde" . 241) ("ograve" . 242) ("oacute" . 243)
+	       ("ocirc" . 244) ("otilde" . 245) ("ouml" . 246) ("divide" . 247)
+	       ("oslash" . 248) ("ugrave" . 249) ("uacute" . 250) ("ucirc" . 251)
+	       ("uuml" . 252) ("yacute" . 253) ("thorn" . 254) ("yuml" . 255))))
+	(append basic-entity-alist
+		(mapcar
+		 (function
+		  (lambda (entity)
+		    (cons (car entity)
+			  (char-to-string
+			   (make-char 'latin-iso8859-1 (cdr entity))))))
+		 latin1-entity))))))
 (defconst w3m-entity-regexp
   (eval-when-compile
     (format "&\\(%s\\|#[0-9]+\\);?"
