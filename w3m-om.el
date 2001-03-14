c6d4a3de502dc625bae9c4dc4e325d1068b66152
Katsumi Yamaoka <yamaoka@jpl.org>
2001-03-14 04:16:16 +0000

    * w3m-om.el: Rewrite to insert "-" into the name of eol-type variants of each  newly generated coding-system.

--- w3m-om.el
+++ w3m-om.el
@@ -1,4 +1,4 @@
-;;; -*- mode: Emacs-Lisp; coding: euc-japan -*-
+;;; w3m-om.el --- Mule 2 specific functions for w3m
 
 ;; Copyright (C) 2001 TSUCHIYA Masatoshi <tsuchiya@pine.kuee.kyoto-u.ac.jp>
 
@@ -20,7 +20,6 @@
 ;; along with w3m.el; if not, write to the Free Software Foundation,
 ;; Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
-
 ;;; Commentary:
 
 ;; This file contains the stuffs to use w3m.el on Mule-2.  For more
@@ -28,39 +27,48 @@
 ;;
 ;;    http://namazu.org/~tsuchiya/emacs-w3m/
 
-
 ;;; Code:
-(require 'poe)
-(require 'poem)
-(require 'pcustom)
 
+(eval-when-compile (require 'cl))
 
-(dolist (elem '((*autoconv*		. undecided)
-		(*ctext*		. ctext)
-		(*euc-china*		. cn-gb-2312)
-		(*euc-japan*		. euc-japan)
-		(*iso-2022-jp*		. iso-2022-jp)
-		(*iso-2022-ss2-7*	. iso-2022-7bit-ss2)
-		(*sjis*			. shift_jis)
-		(*tis620*		. tis-620)))
-  (unless (coding-system-p (cdr elem))
-    (condition-case nil
-	(let* ((info-vector (copy-sequence (get-code (car elem))))
-	       (document (aref info-vector 2))
-	       (id "(generated automatically by `w3m')"))
-	  (copy-coding-system (car elem) (cdr elem))
-	  (aset info-vector 2
-		(if (and (stringp document)
-			 (> (length document) 0))
-		    (if (string-match "\\.[\t\n ]*$" document)
-			(concat (substring document
-					   0 (match-beginning 0))
-				" " id ".")
-		      (concat document " " id "."))
-		  id))
-	  (put (cdr elem) 'coding-system info-vector))
-      (error))))
-
+;; Generate some coding-systems which have a modern name.
+;; No need to contain the eol-type variants in the folloing alist
+;; because they will also be generated for each coding-system.
+(dolist (elem '((*autoconv*	  undecided)
+		(*ctext*	  ctext)
+		(*euc-china*	  cn-gb-2312)
+		(*euc-japan*	  euc-japan)
+		(*iso-2022-jp*	  iso-2022-jp)
+		(*iso-2022-ss2-7* iso-2022-7bit-ss2)
+		(*sjis*		  shift_jis)
+		(*tis620*	  tis-620)))
+  (unless (coding-system-p (cadr elem))
+    (let* ((from (car elem))
+	   (to (cadr elem))
+	   (info-vector (copy-sequence (get-code from)))
+	   (document (aref info-vector 2))
+	   (id "(generated automatically by `w3m')")
+	   i)
+      (aset info-vector 2 (if (and (stringp document)
+				   (> (length document) 0))
+			      (concat document "\n  " id)
+			    id))
+      (set to to)
+      (put to 'coding-system info-vector)
+      (put to 'post-read-conversion (get from 'post-read-conversion))
+      (put to 'pre-write-conversion (get from 'pre-write-conversion))
+      (when (vectorp (get from 'eol-type))
+	(setq i 0)
+	(dolist (variant (append
+			  (put to 'eol-type
+			       (vector (intern (format "%s-unix" to))
+				       (intern (format "%s-dos" to))
+				       (intern (format "%s-mac" to))))
+			  nil))
+	  (set variant variant)
+	  (put variant 'coding-system to)
+	  (put variant 'eol-type (setq i (1+ i))))))))
 
 (provide 'w3m-om)
-;;; w3m-om.el ends here.
+
+;;; w3m-om.el ends here
