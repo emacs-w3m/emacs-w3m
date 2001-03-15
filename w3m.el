d9fd827bbb8780beff9972f061de0168fc8aeacb
Katsumi Yamaoka <yamaoka@jpl.org>
2001-03-15 05:05:25 +0000

    * w3m.el (w3m-charset-coding-system-alist): Use `coding-system-p' if the  function `find-coding-system' is not bound.

--- w3m.el
+++ w3m.el
@@ -347,9 +347,13 @@ In other environment, use 'native."
 	   (x-shift-jis   . shift_jis)
 	   (x-shift_jis   . shift_jis)
 	   (x-sjis        . shift_jis)))
+	(fn (if (fboundp 'find-coding-system)
+		;; It might be unbound at run-time.
+		'find-coding-system
+	      'coding-system-p))
 	dest)
     (while rest
-      (or (find-coding-system (car (car rest)))
+      (or (funcall fn (car (car rest)))
 	  (setq dest (cons (car rest) dest)))
       (setq rest (cdr rest)))
     dest)
