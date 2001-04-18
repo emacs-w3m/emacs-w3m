f0587fda3d0495da6278dbcc40bf248d416c6c35
Masatoshi TSUCHIYA <tsuchiya@namazu.org>
2001-04-18 15:27:15 +0000

    * w3m.el (w3m-setup-header-line): Check `major-mode' to decide whether header line should be set up.
    * mime-w3m.el (mime-w3m-use-header-line): Removed.
    (mime-w3m-preview-text/html): Canceled last change.

--- mime-w3m.el
+++ mime-w3m.el
@@ -50,11 +50,6 @@
 (require 'mime)
 (eval-when-compile (require 'cl))
 
-(defcustom mime-w3m-use-header-line nil
-  "Non-nil activates header-line of w3m."
-  :group 'w3m
-  :type 'boolean)
-
 (defvar mime-w3m-mode-map nil)
 (defvar mime-w3m-message-structure nil)
 (make-variable-buffer-local 'mime-w3m-message-structure)
@@ -90,8 +85,7 @@
        (mime-insert-text-content entity)
        (run-hooks 'mime-text-decode-hook)
        (condition-case err
-	   (let ((w3m-use-header-line mime-w3m-use-header-line))
-	     (w3m-region p (point-max)))
+	   (w3m-region p (point-max))
 	 (error (message (format "%s" err))))
        (put-text-property p (point-max)
 			  (w3m-static-if (featurep 'xemacs)
--- w3m.el
+++ w3m.el
@@ -2829,7 +2829,9 @@ works on Emacs.
 
   (defun w3m-setup-header-line ()
     "Setup header line (emulating Emacs 21)."
-    (when (and w3m-use-header-line w3m-current-url)
+    (when (and w3m-use-header-line
+	       w3m-current-url
+	       (eq 'w3m-mode major-mode))
       (goto-char (point-min))
       (insert "Location: ")
       (put-text-property (point-min) (point)
