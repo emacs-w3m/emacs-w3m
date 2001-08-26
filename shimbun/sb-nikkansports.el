dbef134ecedeb5bb57e75d14fd886596a3c68862
MIYOSHI Masanori <miyoshi@meadowy.org>
2001-08-26 08:20:44 +0000

    * README.shimbun.ja: Add information on nikkansports.
    * shimbun/sb-nikkansports.el: New file.

---
+++ shimbun/sb-nikkansports.el
@@ -0,0 +1,106 @@
+;;; sb-nikkansports.el --- shimbun backend for www.nikkansports.com
+
+;; Author: MIYOSHI Masanori <miyoshi@boreas.dti.ne.jp>
+
+;; Keywords: news
+
+;;; Copyright:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program; if not, you can either send email to this
+;; program's maintainer or write to: The Free Software Foundation,
+;; Inc.; 59 Temple Place, Suite 330; Boston, MA 02111-1307, USA.
+
+;;; Commentary:
+
+;; Original code was nnshimbun.el written by
+;; TSUCHIYA Masatoshi <tsuchiya@namazu.org>.
+
+;;; Code:
+
+(require 'shimbun)
+(require 'sb-text)
+
+(luna-define-class shimbun-nikkansports (shimbun shimbun-text) ())
+
+(defvar shimbun-nikkansports-url "http://www.nikkansports.com/")
+(defvar shimbun-nikkansports-groups-alist
+  '(("baseball" . "baseball/menu-bb.html")
+    ("mlb" . "baseball/topics2/menu-bb.html")
+    ("soccer" . "soccer/menu-sc.html")
+    ("world-soccer" . "soccer/world/menu-wf.html")
+    ("sports" . "sports/menu-sp.html")
+    ("battle" . "battle/menu-bt.html")
+    ("horseracing" . "horserace/menu-hr.html")
+    ("entertainment" . "entert/menu-et.html")
+    ("society" . "society/menu-so.html")
+    ("leisure" . "ls/menu-ls.html")))
+(defvar shimbun-nikkansports-groups
+  (mapcar 'car shimbun-nikkansports-groups-alist))
+(defvar shimbun-nikkansports-from-address "webmast@nikkansports.co.jp")
+(defvar shimbun-nikkansports-content-start
+  "<H2>[^<]+</H2>\n\\(<img[^>]*>\n\\)?")
+(defvar shimbun-nikkansports-content-end
+  "\\(<br><b>[^<]+</b></font>\n\\)?<ul>")
+(defvar shimbun-nikkansports-expiration-days 17)
+
+(luna-define-method shimbun-index-url ((shimbun shimbun-nikkansports))
+  (concat (shimbun-url-internal shimbun)
+	  "news/"
+	  (cdr (assoc (shimbun-current-group-internal shimbun)
+		      shimbun-nikkansports-groups-alist))))
+
+(luna-define-method shimbun-get-headers ((shimbun shimbun-nikkansports)
+					 &optional range)
+  (when (re-search-forward "\n<!--\\( End of Header \\|header\\)-->\n" nil t)
+    (delete-region (point-min) (point))
+    (when (re-search-forward
+	   "\n<!--\\( Start of Footer \\|footer-\\)-->\n" nil t)
+      (forward-line -1)
+      (delete-region (point) (point-max))
+      (goto-char (point-min))
+      (let ((case-fold-search t) headers)
+	(while (re-search-forward
+		"<li><a href=\"\\(.+\\([0-9][0-9]\\)\\([0-9][0-9]\\)\\([0-9][0-9]\\)-\\([0-9]+\\)\\.html\\)\">\\([^<]+\\)</a>" nil t)
+	  (let ((url (match-string 1))
+		(year (match-string 2))
+		(month (match-string 3))
+		(day (match-string 4))
+		(no (match-string 5))
+		(subject (match-string 6))
+		id date)
+	    (setq id (format "<%s%s%s%s.%s@nikkansports.co.jp>"
+			     year month day no
+			     (shimbun-current-group-internal shimbun)))
+	    (setq date (shimbun-make-date-string
+			(string-to-number year)
+			(string-to-number month)
+			(string-to-number day)))
+	    (push (shimbun-make-header
+		   0
+		   (shimbun-mime-encode-string subject)
+		   (shimbun-from-address-internal shimbun)
+		   date id "" 0 0
+		   (concat 
+		    (shimbun-url-internal shimbun)
+		    "news/"
+		    (file-name-directory
+		     (cdr (assoc (shimbun-current-group-internal shimbun)
+				 shimbun-nikkansports-groups-alist)))
+		    url))
+		  headers)))
+	headers))))
+
+(provide 'sb-nikkansports)
+
+;;; sb-nikkansports.el ends here
