8077c2f1bbe6aaaf1ce8ee104b10af9fd9819321
minakaji <>
2001-10-24 23:10:31 +0000

    (shimbun-vinelinux-url) Bug fixed. (shimbun-get-headers): Ditto.

--- shimbun/sb-vinelinux.el
+++ shimbun/sb-vinelinux.el
@@ -29,7 +29,7 @@
 
 (luna-define-class shimbun-vinelinux (shimbun) ())
 
-(defconst shimbun-vinelinux-url "http://www.vinelinux.com")
+(defconst shimbun-vinelinux-url "http://www.vinelinux.org")
 (defconst shimbun-vinelinux-group-path-alist
   '(("errata.2x.i386" . "errata/2x/i386.html")
     ("errata.2x.ppc" . "errata/2x/ppc.html")
@@ -63,43 +63,47 @@
 (luna-define-method shimbun-get-headers ((shimbun shimbun-vinelinux)
 					 &optional range)
   (let ((case-fold-search t)
-	(start (progn (search-forward "</font>の更新/障害情報です</h4>" nil t nil)
-		      (search-forward "<table>" nil t nil)
-		      (point)))
-	(end (progn (search-forward "</table>" nil t nil)
-		    (point)))
 	;;(pages (shimbun-header-index-pages range))
 	(count 0)
-	headers aux url id date subject)
-    (goto-char start)
-    ;; Use entire archive.
-    (catch 'stop
-      (while ;;(and (if pages (<= (incf count) pages) t)
-	  (re-search-forward
-	   ;; <td><a href="20010501.html">quota の更新</a></td>
-	   ;; <td><a href="20010501-3.html">Vine Linux 2.1.5 にアップグレードした時の xdvi や tgif 等の日本語表示の不具合</a></td>
-	   "^<td><a href=\"\\(\\([0-9]+\\)\\(-[0-9]+\\)*\\.html\\)\">\\(.+\\)</a></td>"
-	   end t)
-	;;)
-	(setq url (concat shimbun-vinelinux-url
-			  "/"
-			  (cdr (assoc (shimbun-current-group-internal shimbun)
-				      shimbun-vinelinux-group-path-alist1))
-			  (match-string-no-properties 1))
-	      id (format "<%s%05d%%%s>"
-			 (match-string-no-properties 1)
-			 (string-to-number (match-string-no-properties 2))
-			 (shimbun-current-group-internal shimbun))
-	      date (shimbun-vinelinux-parse-time (match-string-no-properties 2))
-	      ;; from (match-string 4)
-	      subject (match-string 4))
-	(if (shimbun-search-id shimbun id)
-	    (throw 'stop nil))
-	(forward-line 1)
-	(push (shimbun-make-header
-	       0 (shimbun-mime-encode-string subject)
-	       shimbun-vinelinux-from-address date id "" 0 0 url)
-	      headers)))
+	start end headers aux url id date subject)
+    (with-temp-buffer
+      (shimbun-retrieve-url (shimbun-index-url shimbun) 'reload)
+      (subst-char-in-region (point-min) (point-max) ?\t ?  t)
+      (setq start (progn
+		    (search-forward "</font>の更新/障害情報です</h4>" nil t nil)
+		    (search-forward "<table>" nil t nil)
+		    (point))
+	    end (progn (search-forward "</table>" nil t nil)
+		       (point)))
+      (goto-char start)
+      ;; Use entire archive.
+      (catch 'stop
+	(while ;;(and (if pages (<= (incf count) pages) t)
+	    (re-search-forward
+	     ;; <td><a href="20010501.html">quota の更新</a></td>
+;; <td><a href="20010501-3.html">Vine Linux 2.1.5 にアップグレードした時の xdvi や tgif 等の日本語表示の不具合</a></td>
+	     "^<td><a href=\"\\(\\([0-9]+\\)\\(-[0-9]+\\)*\\.html\\)\">\\(.+\\)</a></td>"
+	     end t)
+	  ;;)
+	  (setq url (concat shimbun-vinelinux-url
+			    "/"
+			    (cdr (assoc (shimbun-current-group-internal shimbun)
+					shimbun-vinelinux-group-path-alist1))
+			    (match-string-no-properties 1))
+		id (format "<%s%05d%%%s>"
+			   (match-string-no-properties 1)
+			   (string-to-number (match-string-no-properties 2))
+			   (shimbun-current-group-internal shimbun))
+		date (shimbun-vinelinux-parse-time (match-string-no-properties 2))
+		;; from (match-string 4)
+		subject (match-string 4))
+	  (if (shimbun-search-id shimbun id)
+	      (throw 'stop nil))
+	  (forward-line 1)
+	  (push (shimbun-make-header
+		 0 (shimbun-mime-encode-string subject)
+		 shimbun-vinelinux-from-address date id "" 0 0 url)
+		headers))))
     headers))
 
 (luna-define-method shimbun-make-contents ((shimbun shimbun-vinelinux) header)
