5e221d6834185051b06b4e0c270b8dc7a736c40a
Katsumi Yamaoka <yamaoka@jpl.org>
2001-10-29 00:54:55 +0000

    Fix.

--- w3m.el
+++ w3m.el
@@ -2838,7 +2838,7 @@ to nil.
 			   (japanese-jisx0212 . lc-jp2)
 			   (katakana-jisx0201 . lc-kana))))
 		    (eval (cdr (assq charset alist))))
-		(eval (list 'charset-id (quote charset))))))
+		(eval '(charset-id charset)))))
       (if (fboundp 'ccl-compile-write-multibyte-character)
 	  (` ((r0 = (, id))
 	      (write-multibyte-character r0 r1)
