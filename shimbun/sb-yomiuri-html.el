873902653e6fa02b0a3923289773c65e006c1262
Masatoshi TSUCHIYA <tsuchiya@namazu.org>
2001-11-06 03:02:56 +0000

    This commit was manufactured by cvs2svn to create branch 'emacs-w3m-1_2'.
    Cherrypick from master 2001-11-06 03:02:55 UTC teranisi '* shimbun/sb-yomiuri-html.el: New file.':
        shimbun/sb-yomiuri-html.el

---
+++ shimbun/sb-yomiuri-html.el
@@ -0,0 +1,44 @@
+;;; sb-yomiuri-html.el --- shimbun backend for yomiuri online (HTML version)
+
+;; Author: Yuuichi Teranishi <teranisi@gohome.org>
+
+;; Keywords: news
+
+;;; Copyright:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program; if not, you can either send email to this
+;; program's maintainer or write to: The Free Software Foundation,
+;; Inc.; 59 Temple Place, Suite 330; Boston, MA 02111-1307, USA.
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'shimbun)
+(require 'sb-yomiuri)
+
+(luna-define-class shimbun-yomiuri-html (shimbun shimbun-yomiuri) ())
+
+(defvar shimbun-yomiuri-html-url "http://www.yomiuri.co.jp/")
+(defvar shimbun-yomiuri-html-groups '("shakai" "sports" "seiji" "keizai"
+				 "kokusai" "fuho"))
+(defvar shimbun-yomiuri-html-from-address  "webmaster@www.yomiuri.co.jp")
+(defvar shimbun-yomiuri-html-content-start "\n<!--  photo start  -->\n")
+(defvar shimbun-yomiuri-html-content-end  "\n<!--  honbun end  -->\n")
+
+(defvar shimbun-yomiuri-html-expiration-days 7)
+
+(provide 'sb-yomiuri-html)
+
+;;; sb-yomiuri-html.el ends here
