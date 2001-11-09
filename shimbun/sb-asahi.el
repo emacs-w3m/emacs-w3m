8cfc380383179345026887a9f0e9e199d95035bc
Katsumi Yamaoka <yamaoka@jpl.org>
2001-11-09 08:54:05 +0000

    * sb-asahi.el (shimbun-get-headers): Look for the new news in the list page  of a group if a group is a member of  `shimbun-asahi-groups-should-use-list-page'. (shimbun-asahi-get-headers-in-the-top-page): New function. (shimbun-asahi-get-headers-in-the-list-page): New function. (shimbun-index-url): Use the list page of a group if a group is a member of  `shimbun-asahi-groups-should-use-list-page'. (shimbun-asahi-groups-should-use-list-page): New variable.

--- shimbun/sb-asahi.el
+++ shimbun/sb-asahi.el
@@ -34,9 +34,20 @@
 (require 'sb-text)
 (luna-define-class shimbun-asahi (shimbun-text) ())
 
-(defvar shimbun-asahi-url "http://spin.asahi.com/")
+(defvar shimbun-asahi-url "http://spin.asahi.com/"
+  "Name of the parent url.")
+
 (defvar shimbun-asahi-groups '("national" "business" "politics"
-			       "international" "sports" "culture"))
+			       "international" "sports" "culture")
+  "List of available group names.  Each name should be a directory name
+which is in existence under the parent url `shimbun-asahi-url'.")
+
+(defvar shimbun-asahi-groups-should-use-list-page '("business" "sports")
+  "List of groups that the new news can be found in the list page.  If a
+group is included in this list, this program will look for the new
+news in the url \"update/list.html\" under a group directory rather than
+the top page of a group.")
+
 (defvar shimbun-asahi-from-address "webmaster@www.asahi.com")
 
 (defvar shimbun-asahi-content-start "\n<!-- Start of kiji -->\n")
@@ -49,13 +60,70 @@ bIy3rr^<Q#lf&~ADU:X!t5t>gW5)Q]N{Mmn\n L]suPpL|gFjV{S|]a-:)\\FR\
 (defvar shimbun-asahi-expiration-days 6)
 
 (luna-define-method shimbun-index-url ((shimbun shimbun-asahi))
-  (concat (shimbun-url-internal shimbun)
-	  (shimbun-current-group-internal shimbun)
-	  "/"))
+  (let ((group (shimbun-current-group-internal shimbun)))
+    (concat (shimbun-url-internal shimbun)
+	    group
+	    "/"
+	    (if (member group shimbun-asahi-groups-should-use-list-page)
+		"update/list.html"
+	      ""))))
 
-(luna-define-method shimbun-get-headers ((shimbun shimbun-asahi)
-					 &optional range)
-  (let* ((group (shimbun-current-group-internal shimbun))
+(defun shimbun-asahi-get-headers-in-the-list-page (entity)
+  "Return a list of header objects from the list page."
+  (when (search-forward "\n<!-- Start of past -->\n" nil t)
+    (delete-region (point-min) (point))
+    (when (search-forward "\n<!-- End of past -->\n" nil t)
+      (forward-line -1)
+      (delete-region (point) (point-max))
+      (goto-char (point-min))
+      (let ((case-fold-search t)
+	    headers)
+	(while (re-search-forward "\
+<a href=\"\\(\\([0-9][0-9][0-9][0-9]\\)/\\([0-9]+\\)\\.html\\)\"> *"
+				  nil t)
+	  (let* ((group (shimbun-current-group-internal entity))
+		(id (format "<%s%s%%%s>"
+			    (match-string 2) (match-string 3) group))
+		(url (match-string 1)))
+	    (push (shimbun-make-header
+		   0
+		   (shimbun-mime-encode-string
+		    (mapconcat 'identity
+			       (split-string
+				(buffer-substring
+				 (match-end 0)
+				 (progn
+				   (search-forward "<br>" nil t) (point)))
+				"\\(<[^>]+>\\|\r\\)")
+			       ""))
+		   (shimbun-from-address-internal entity)
+		   "" id "" 0 0 (format "%s%s/update/%s"
+					(shimbun-url-internal entity)
+					group url))
+		  headers)))
+	(setq headers (nreverse headers))
+	(let ((i 0))
+	  (while (and (nth i headers)
+		      (re-search-forward "\
+^(\\([0-9][0-9]\\)/\\([0-9][0-9]\\) \\([0-9][0-9]:[0-9][0-9]\\))"
+					 nil t))
+	    (let ((month (string-to-number (match-string 1)))
+		  (date (decode-time (current-time))))
+	      (shimbun-header-set-date
+	       (nth i headers)
+	       (shimbun-make-date-string
+		(if (and (eq 12 month) (eq 1 (nth 4 date)))
+		    (1- (nth 5 date))
+		  (nth 5 date))
+		month
+		(string-to-number (match-string 2))
+		(match-string 3))))
+	    (setq i (1+ i))))
+	(nreverse headers)))))
+
+(defun shimbun-asahi-get-headers-in-the-top-page (entity)
+  "Return a list of header objects from the top page."
+  (let* ((group (shimbun-current-group-internal entity))
 	 (regexp (concat "<a[\t\n ]+href=[\t\n ]*\"/\\("
 			 (regexp-quote group)
 			 "/update/\\([0-9][0-9][0-9][0-9]\\)"
@@ -89,7 +157,7 @@ bIy3rr^<Q#lf&~ADU:X!t5t>gW5)Q]N{Mmn\n L]suPpL|gFjV{S|]a-:)\\FR\
 	 (shimbun-make-header
 	  0
 	  subject
-	  (shimbun-from-address-internal shimbun)
+	  (shimbun-from-address-internal entity)
 	  (if month
 	      (shimbun-make-date-string (if (and (eq 12 month)
 						 (eq 1 (nth 4 date)))
@@ -100,9 +168,16 @@ bIy3rr^<Q#lf&~ADU:X!t5t>gW5)Q]N{Mmn\n L]suPpL|gFjV{S|]a-:)\\FR\
 					(match-string 3))
 	    "")
 	  id "" 0 0
-	  (concat (shimbun-url-internal shimbun) url))
+	  (concat (shimbun-url-internal entity) url))
 	 headers)))
     headers))
+
+(luna-define-method shimbun-get-headers ((shimbun shimbun-asahi)
+					 &optional range)
+  (if (member (shimbun-current-group-internal shimbun)
+	      shimbun-asahi-groups-should-use-list-page)
+      (shimbun-asahi-get-headers-in-the-list-page shimbun)
+    (shimbun-asahi-get-headers-in-the-top-page shimbun)))
 
 (provide 'sb-asahi)
 
