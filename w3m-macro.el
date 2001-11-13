2525f04c6f0d1e0fdade763bd090693dca084e05
Katsumi Yamaoka <yamaoka@jpl.org>
2001-11-13 10:25:15 +0000

    * w3m-macro.el (w3m-condition-case): New macro.

--- w3m-macro.el
+++ w3m-macro.el
@@ -70,6 +70,16 @@ compile-time."
   (if clauses
       (cons 'progn (cdr (car clauses)))))
 
+(put 'w3m-condition-case lisp-indent-function 2)
+(defmacro w3m-condition-case (var bodyform &rest handlers)
+  "Like `condition-case', except that signal an error if `debug-on-error'
+or `debug-on-quit' is non-nil."
+  `(if (or debug-on-error debug-on-quit)
+       ,bodyform
+     (condition-case ,var
+	 ,bodyform
+       ,@handlers)))
+
 (defmacro w3m-add-text-properties (start end props &optional object)
   "Like `add-text-properties' but always add the non-sticky properties."
   (let ((non-stickies
