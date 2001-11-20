dc52b478c979358407c89dce78080124ca004b3d
Masatoshi TSUCHIYA <tsuchiya@namazu.org>
2001-11-20 04:52:52 +0000

    Slightly modified to fix the last change.

--- shimbun/sb-security-memo.el
+++ shimbun/sb-security-memo.el
@@ -3,10 +3,9 @@
 ;; Copyright (C) 2001 TSUCHIYA Masatoshi <tsuchiya@namazu.org>
 
 ;; Author: TSUCHIYA Masatoshi <tsuchiya@namazu.org>
-
 ;; Keywords: news
 
-;;; Copyright:
+;; This file is a part of shimbun.
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
