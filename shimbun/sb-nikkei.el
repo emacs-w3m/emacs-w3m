d440d1b69371c2e0b4924cbf0f08f387420955fd
Katsumi Yamaoka <yamaoka@jpl.org>
2001-11-27 06:34:23 +0000

    * sb-nikkei.el (shimbun-get-headers): Make directory separator not duplicated.

--- shimbun/sb-nikkei.el
+++ shimbun/sb-nikkei.el
@@ -67,7 +67,7 @@ Lt}?~IId8Jj&vP^3*o=LKUyk(`t%0c!;t6REk=JbpsEn9MrN7gZ%")))
 	     (day   (string-to-number (match-string 5)))
 	     (time  (or (match-string 8) "00:00"))
 	     (group (shimbun-current-group-internal shimbun))
-	     (url (format "%s/%s/%s" (shimbun-url-internal shimbun) group file))
+	     (url (format "%s%s/%s" (shimbun-url-internal shimbun) group file))
 	     (title (match-string 9)))
 	(push (shimbun-make-header
 	       0
