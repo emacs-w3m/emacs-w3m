f42ceabc913be054ff717dc380263c185efc8c9d
Masatoshi TSUCHIYA <tsuchiya@namazu.org>
2002-01-11 07:52:25 +0000

    (w3m-region): Process <base> tag.

--- w3m.el
+++ w3m.el
@@ -4719,6 +4719,12 @@ ex.) c:/dir/file => //c/dir/file"
 	  w3m-current-url url
 	  w3m-current-base-url url
 	  w3m-current-title (w3m-rendering-multibyte-buffer))
+    (unless w3m-current-base-url
+      (let ((case-fold-search t))
+	(goto-char (point-min))
+	(when (re-search-forward "<base[ \t\r\f\n]+" nil t)
+	  (w3m-parse-attributes (href)
+	    (setq w3m-current-base-url href)))))
     (w3m-fontify)
     (when (w3m-display-inline-images-p)
       (and w3m-force-redisplay (sit-for 0))
