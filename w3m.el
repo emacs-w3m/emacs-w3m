a147b104cfba27e193cd2ef48e9425fd5e3ec679
Katsumi Yamaoka <yamaoka@jpl.org>
2002-04-15 03:44:03 +0000

    Typo.

--- w3m.el
+++ w3m.el
@@ -630,7 +630,7 @@ of the original request method. -- RFC2616"
 		'face-custom-attributes-get
 	      'custom-face-attributes-get));; What a perverseness it is.
 	;; `custom-face-attributes-get' in CUSTOM 1.9962 attempts to require
-	;; `font' in Emacs/W3, and it requires `cl' unconditionally. :-(
+	;; `font' in Emacs/w3, and it requires `cl' unconditionally. :-(
 	(features (cons 'font features))
 	base-attributes attributes attribute)
     (require 'wid-edit);; Needed for only Emacs 20.
