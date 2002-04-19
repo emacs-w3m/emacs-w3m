782a9cc836e03833443f69dc0c247d97619d0c6f
Katsumi Yamaoka <yamaoka@jpl.org>
2002-04-19 04:39:42 +0000

    * shimbun.el (shimbun-insert-string): Avoid compile warn.

--- shimbun/shimbun.el
+++ shimbun/shimbun.el
@@ -455,12 +455,12 @@ If OUTBUF is not specified, article is retrieved to the current buffer.")
 Point and before-insertion markers move forward to end up
 after the inserted text.
 Any other markers at the point of insertion remain before the text."
-  `(if (featurep 'xemacs)
-       (insert ,string)
-     (insert
-      (if enable-multibyte-characters
-	  (string-as-multibyte ,string)
-	(string-as-unibyte ,string)))))
+  (if (and (fboundp 'string-as-multibyte)
+	   (subrp (symbol-function 'string-as-multibyte)))
+      `(insert (if enable-multibyte-characters
+		   (string-as-multibyte ,string)
+		 (string-as-unibyte ,string)))
+    `(insert ,string)))
 
 (luna-define-method shimbun-article ((shimbun shimbun) header &optional outbuf)
   (when (shimbun-current-group-internal shimbun)
