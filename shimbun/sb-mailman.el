580191bb59e486916b87ce61e80b43a75c2f7719
Katsumi Yamaoka <yamaoka@jpl.org>
2002-05-20 10:49:50 +0000

    * sb-mailman.el (shimbun-mailman-make-contents): New function separated from  `shimbun-make-contents'. (shimbun-make-contents): Use it. (shimbun-mailman-headers): New function separated from `shimbun-headers';  rewrite it to search new articles in the reverse order.

--- shimbun/sb-mailman.el
+++ shimbun/sb-mailman.el
@@ -2,7 +2,8 @@
 
 ;; Copyright (C) 2002 NAKAJIMA Mikio <minakaji@osaka.email.ne.jp>
 
-;; Author: NAKAJIMA Mikio <minakaji@osaka.email.ne.jp>
+;; Authors: NAKAJIMA Mikio  <minakaji@osaka.email.ne.jp>,
+;;          Katsumi Yamaoka <yamaoka@jpl.org>
 ;; Keywords: news
 
 ;; This file is a part of shimbun.
@@ -23,6 +24,7 @@
 ;; Inc.; 59 Temple Place, Suite 330; Boston, MA 02111-1307, USA.
 
 ;;; Commentary:
+
 ;; Mailman is the GNU Mailing List Manager.
 ;; See http://www.gnu.org/software/mailman/index.html for its detail.
 
@@ -35,26 +37,26 @@
 
 (luna-define-class shimbun-mailman (shimbun) ())
 
-(luna-define-method shimbun-make-contents ((shimbun shimbun-mailman) header)
-  (subst-char-in-region (point-min) (point-max) ?\t ?  t)
+(defun shimbun-mailman-make-contents (shimbun header)
+  (subst-char-in-region (point-min) (point-max) ?\t ?\  t)
   (shimbun-decode-entities)
   (goto-char (point-min))
-  (let ((end (search-forward "<!--beginarticle-->"))
-	value)
+  (let ((end (search-forward "<!--beginarticle-->")))
     (goto-char (point-min))
     (search-forward "</HEAD>")
     (when (re-search-forward "<H1>\\([^\n]+\\)\n +</H1>" end t nil)
       (shimbun-header-set-subject
        header
        (shimbun-mime-encode-string (match-string 1))))
-    (when (re-search-forward
-	   "<B>\\([^\n]+\\)\n +</B> *\n +<A HREF=\"[^\n]+\n +TITLE=\"[^\n]+\">\\([^\n]+\\)"
-	   end t nil)
+    (when (re-search-forward "<B>\\([^\n]+\\)\n +</B> *\n +\
+<A HREF=\"[^\n]+\n +TITLE=\"[^\n]+\">\\([^\n]+\\)"
+			     end t nil)
       ;;(list (match-string 1) (match-string 2)))
       ;; ("Travis Casey" "efindel@earthlink.net")
       (shimbun-header-set-from
        header
-       (shimbun-mime-encode-string (concat (match-string 1) " <" (match-string 2) ">")))
+       (shimbun-mime-encode-string (concat (match-string 1)
+					   " <" (match-string 2) ">")))
       (when (re-search-forward "<I>\\([^\n]+\\)</I>" end t nil)
 	(shimbun-header-set-date header (match-string 1)))
       (delete-region (point-min) end)
@@ -66,64 +68,65 @@
     (encode-coding-string (buffer-string)
 			  (mime-charset-to-coding-system "ISO-2022-JP"))))
 
-(luna-define-method shimbun-headers ((shimbun shimbun-mailman) &optional range)
+(luna-define-method shimbun-make-contents ((shimbun shimbun-mailman) header)
+  (shimbun-mailman-make-contents shimbun header))
+
+(defun shimbun-mailman-headers (shimbun range)
   (with-temp-buffer
-    (shimbun-retrieve-url
-     (concat (shimbun-index-url shimbun) "/index.html") 'reload)
-    (let ((case-fold-search t)
-	  (pages (shimbun-header-index-pages range))
-	  (count 0)
-	  headers auxs aux)
-      (while (and (if pages (<= (incf count) pages) t)
-		  ;;<A href="2002-April/date.html">[ Date ]</a>
-		  (re-search-forward "<a href=\"\\([0-9][0-9][0-9][0-9]-\\(January\\|February\\|March\\|April\\|May\\|June\\|July\\|August\\|September\\|October\\|November\\|December\\|\\)\\)/date.html\">" nil t))
-	;; (list (match-string 1) (match-string 2))
-	;;("2002-April" "April")
-	(setq auxs (append auxs (list (match-string 1)))))
+    (let* ((index-url (shimbun-index-url shimbun))
+	   (group (shimbun-current-group-internal shimbun))
+	   (suffix (if (string-match "^http://\\([^/]+\\)/" index-url)
+		       (match-string 1 index-url)
+		     index-url))
+	   auxs aux id url subject from headers)
+      (shimbun-retrieve-url (concat index-url "/index.html") 'reload)
+      (setq case-fold-search t)
+      (let ((pages (shimbun-header-index-pages range))
+	    (count 0))
+	(while (and (if pages (<= (incf count) pages) t)
+		    (re-search-forward "<a href=\"\\(20[0-9][0-9]-\
+\\(January\\|February\\|March\\|April\\|May\\|June\
+\\|July\\|August\\|September\\|October\\|November\\|December\\)\
+\\)/date.html\">"
+				       nil t))
+	  (push (match-string 1) auxs)))
+      (setq auxs (nreverse auxs))
       (catch 'stop
 	(while auxs
-	  (with-temp-buffer
-	    (shimbun-retrieve-url
-	     (concat (shimbun-index-url shimbun)
-		     "/" (setq aux (car auxs)) "/date.html")
-	     'reload)
-	    (subst-char-in-region (point-min) (point-max) ?\t ?  t)
-	    (let ((case-fold-search t)
-		  (suffix (save-match-data
-			    (if (string-match "^http://\\([^/]+\\)/"
-					      (shimbun-index-url shimbun))
-				(match-string 1 (shimbun-index-url shimbun))
-			      (shimbun-index-url shimbun)))))
-	      (goto-char (point-min))
-	      (while (re-search-forward
-		      "<LI><A HREF=\"\\(\\([0-9]+\\)\\.html\\)\">\\([^\n]+\\)\n</A><A NAME=\"[0-9]+\">&nbsp;</A>\n<I>\\([^\n]+\\)\n</I>"
-		      nil t nil)
-		;;(list (match-string 1)(match-string 2)(match-string 3)(match-string 4))
-		;;("000128.html" "000128" "[newbie] Palm m505 usb sync: Eureka! success" "Dan Butler")
-		;;
-		;;http://www.pilot-link.org/pipermail/pilot-link-general/2002-April/000122.html
-		(let* ((url (concat (shimbun-index-url shimbun)
-				    "/" aux "/" (match-string 1)))
-		       (id (format "<%06d.%s@%s>"
-				   (string-to-number (match-string 2))
-				   (shimbun-current-group-internal shimbun)
-				   suffix))
-		       (subject (match-string 3))
-		       (from (match-string 4)))
-		  (if (shimbun-search-id shimbun id)
-		      (throw 'stop nil))
-		  (setq subject (with-temp-buffer
-				  (insert subject)
-				  (shimbun-decode-entities)
-				  (shimbun-remove-markup)
-				  (buffer-string)))
-		  (push (shimbun-make-header
-			 0
-			 (shimbun-mime-encode-string subject)
-			 from "" id "" 0 0 url)
-			headers))
-		(setq auxs (cdr auxs)))))))
-      (nreverse headers))))
+	  (erase-buffer)
+	  (shimbun-retrieve-url (concat index-url "/"
+					(setq aux (car auxs))
+					"/date.html")
+				'reload)
+	  (subst-char-in-region (point-min) (point-max) ?\t ?\  t)
+	  (goto-char (point-max))
+	  (while (re-search-backward "<LI><A HREF=\"\\(\\([0-9]+\\)\\.html\\)\
+\">\\([^\n]+\\)\n</A><A NAME=\"[0-9]+\">&nbsp;</A>\n<I>\\([^\n]+\\)\n</I>"
+				     nil t)
+	    (setq id (format "<%06d.%s@%s>"
+			     (string-to-number (match-string 2))
+			     group
+			     suffix))
+	    (when (shimbun-search-id shimbun id)
+	      (throw 'stop nil))
+	    (setq url (concat index-url "/" aux "/" (match-string 1))
+		  subject (match-string 3)
+		  from (match-string 4))
+	    (setq subject (with-temp-buffer
+			    (insert subject)
+			    (shimbun-decode-entities)
+			    (shimbun-remove-markup)
+			    (buffer-string)))
+	    (push (shimbun-make-header
+		   0
+		   (shimbun-mime-encode-string subject)
+		   from "" id "" 0 0 url)
+		  headers))
+	  (setq auxs (cdr auxs))))
+      headers)))
+
+(luna-define-method shimbun-headers ((shimbun shimbun-mailman) &optional range)
+  (shimbun-mailman-headers shimbun range))
 
 (provide 'sb-mailman)
 
