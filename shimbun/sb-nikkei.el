84f26ad2731b514117ddd432992409c457d94a07
Katsumi Yamaoka <yamaoka@jpl.org>
2002-05-20 11:33:22 +0000

    * sb-nikkei.el (shimbun-nikkei-groups): Add `main'. From NOMIYA-san <nomiya@ttmy.ne.jp>.

--- shimbun/sb-nikkei.el
+++ shimbun/sb-nikkei.el
@@ -38,7 +38,9 @@
 
 (defvar shimbun-nikkei-url "http://www.nikkei.co.jp/news/")
 
-(defvar shimbun-nikkei-groups '("keizai" "seiji" "kaigai" "market" "sangyo" "tento" "shakai" "retto" "shasetsu" "zinzi" "okuyami"))
+(defvar shimbun-nikkei-groups
+  '("main" "keizai" "seiji" "kaigai" "market" "sangyo" "tento" "shakai"
+    "retto" "shasetsu" "zinzi" "okuyami"))
 
 (defvar shimbun-nikkei-from-address "webmaster@nikkei.co.jp")
 (defvar shimbun-nikkei-content-start "<!--FJZONE START NAME=\"HONBUN\" -->")
