445d462c26512230e62173ca09cbfab441fb359d
Katsumi Yamaoka <yamaoka@jpl.org>
2002-07-09 06:01:27 +0000

    Applied patch from Hironori FUJII <fujii@chi.its.hiroshima-cu.ac.jp>. * w3m.el (w3m-highlight-current-anchor): Don't infloop at the beginning of  a buffer.

--- w3m.el
+++ w3m.el
@@ -4239,9 +4239,8 @@ Return t if current line has a same anchor sequence."
 	  (while (w3m-highlight-current-anchor-1 seq)
 	    (forward-line 1))
 	  (goto-char pos)
-	  (forward-line -1)
-	  (while (w3m-highlight-current-anchor-1 seq)
-	    (forward-line -1)))))))
+	  (while (and (zerop (forward-line -1))
+		      (w3m-highlight-current-anchor-1 seq))))))))
 
 (defun w3m-edit-url (url)
   "Edit the local file pointed by URL."
