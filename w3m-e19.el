eb82c625c26f49943cf922f2fc70b26d4b3e7786
Katsumi Yamaoka <yamaoka@jpl.org>
2002-10-21 15:10:46 +0000

    * w3m-e19.el: Cancel last change for providing the base64 codec.

--- w3m-e19.el
+++ w3m-e19.el
@@ -43,8 +43,6 @@
 (eval-when-compile
   (require 'cl))
 
-(require 'poe)
-(require 'path-util)
 (require 'pcustom)
 (require 'regexp-opt)
 
@@ -91,34 +89,6 @@ the empty string."
   (if (and (equal ad-return-value "")
 	   default-value)
       (setq ad-return-value default-value)))
-
-(unless (and (fboundp 'base64-decode-string)
-	     (fboundp 'base64-encode-string))
-  (cond ((locate-library "base64")
-	 (autoload 'base64-decode-string "base64")
-	 (autoload 'base64-encode-string "base64"))
-	((exec-installed-p "mmencode")
-	 (defalias 'base64-decode-string
-	   (function
-	    (lambda (string)
-	      "Base64-decode STRING and return the result."
-	      (with-temp-buffer
-		(insert string)
-		(call-process-region (point-min) (point-max)
-				     "mmencode" t t nil "-u")
-		(buffer-string)))))
-	 (defalias 'base64-encode-string
-	   (function
-	    (lambda (string)
-	      "Base64 encode STRING and return the result."
-	      (with-temp-buffer
-		(insert string)
-		(call-process-region (point-min) (point-max)
-				     "mmencode" t t)
-		(buffer-string))))))
-	(t
-	 (defalias 'base64-decode-string 'identity)
-	 (defalias 'base64-encode-string 'identity))))
 
 (provide 'w3m-e19)
 
