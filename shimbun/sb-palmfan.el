9db302a171f5f11e512731b88d6f3ab386172f25
minakaji <>
2002-12-08 10:43:29 +0000

    (shimbun-palmfan-date-regexp): Modify regexp. (shimbun-palmfan-news-headers): Pick up MONTH, DAY and YEAR properly.

--- shimbun/sb-palmfan.el
+++ shimbun/sb-palmfan.el
@@ -46,7 +46,7 @@
 (defconst shimbun-palmfan-date-regexp
   ;;<P><A name="Apr,13.2002"></A><B>　Apr,13.2002</B><A href="#Apr,12.2002">▼</A>
   ;;"^<P><A name=\"\\(Jan\\|Feb\\|Mar\\|Apr\\|May\\|Jun\\|Jul\\|Aug\\|Sep\\|Oct\\|Nov\\|Dec\\|\\),\\([0-9]+\\)\\.\\([0-9]+\\)\"></A><B>.*▼</A>$")
-  "^<!-- 日付 -->\n+.*\\(January\\|February\\|March\\|April\\|May\\|June\\|July\\|August\\|September\\|October\\|November\\|December\\),\\([0-9]+\\)\\.\\([0-9]+\\)")
+  "^<!-- 日付 -->\n+.*\\(Sun\\|Mon\\|Tue\\|Wed\\|Thr\\|Fri\\|Sat\\),* \\(January\\|February\\|March\\|April\\|May\\|June\\|July\\|August\\|September\\|October\\|November\\|December\\|Jan\\|Feb\\|Mar\\|Apr\\|May\\|Jun\\|Jul\\|Aug\\|Sep\\|Oct\\|Nov\\|Dec\\) \\([0-9]+\\)\\.\\([0-9]+\\)")
 
 (defconst shimbun-palmfan-palmwarefan-date-regexp
   "<!-- \\([0-9][0-9][0-9][0-9]\\)/\\([0-9][0-9]*\\)/\\([0-9][0-9]*\\) -->$")
@@ -54,7 +54,10 @@
 (defconst shimbun-palmfan-month-alist
   '(("January" . 1) ("February" . 2) ("March" . 3) ("April" . 4)
     ("May" . 5) ("June" . 6) ("July" . 7) ("August" . 8)
-    ("September" . 9) ("October" . 10) ("November" . 11) ("December" . 12)))
+    ("September" . 9) ("October" . 10) ("November" . 11) ("December" . 12)
+    ("Jan" . 1) ("Feb" . 2) ("Mar" . 3) ("Apr" . 4)
+    ("May" . 5) ("Jun" . 6) ("Jul" . 7) ("Aug" . 8)
+    ("Sep" . 9) ("Oct" . 10) ("Nov" . 11) ("Dec" . 12)))
 
 (luna-define-method initialize-instance :after ((shimbun shimbun-palmfan)
 						&rest init-args)
@@ -217,9 +220,9 @@
       (goto-char (point-min))
       (catch 'stop
 	(while (re-search-forward shimbun-palmfan-date-regexp nil t nil)
-	  (let* ((month (match-string 1))
-		 (day (string-to-number (match-string 2)))
-		 (year (string-to-number (match-string 3)))
+	  (let* ((month (match-string 2))
+		 (day (string-to-number (match-string 3)))
+		 (year (string-to-number (match-string 4)))
 		 (date (format "%02d %s %04d 00:00 +0900" day month year))
 		 (start (point))
 		 (end (progn
