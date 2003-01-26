5e075c2f418ea18bbbbbce786aa90fa97632a275
MIYOSHI Masanori <miyoshi@meadowy.org>
2003-01-26 06:48:07 +0000

    (w3m-favicon-type): Add BMP at the head of the list. (w3m-favicon-type): Undo the last change and make the priority of BMP the lowest.

--- w3m-e21.el
+++ w3m-e21.el
@@ -378,11 +378,7 @@ If this variable is nil, never expired."
   :type 'integer)
 
 
-;; In the implementation of Meadow2, BMP and bmp are different.
-;;   o BMP: the ImageMagick decoder is used.
-;;   o bmp: the built-in decoder that is slightly poor is used.
-;; So I modify the order of priority.
-(defcustom w3m-favicon-type (let ((types '(BMP bmp pbm png gif xpm)))
+(defcustom w3m-favicon-type (let ((types '(pbm png gif xpm bmp)))
 			      (catch 'det
 				(while types
 				  (when (image-type-available-p (car types))
