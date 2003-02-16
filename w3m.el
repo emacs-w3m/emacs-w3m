0ef4699f79be8ba25a93018106e147363d9ab05c
ARISAWA Akihiro <ari@mbf.sphere.ne.jp>
2003-02-16 00:51:52 +0000

    (w3m-type): Support "release candidate" of w3m.

--- w3m.el
+++ w3m.el
@@ -187,14 +187,15 @@ These values are acceptable: w3m, w3mmee, w3m-m17n.")
 	(with-temp-buffer
 	  (call-process command nil t nil "-version")
 	  (goto-char (point-min))
-	  (when (re-search-forward "version \\(w3m/0\\.3\
-\\(\\.[0-9\\]\\)*\\(-stable\\|\\(\\+cvs\\(-[0-9]+\\.[0-9]+\\)?\\)\\)?\
+	  (when (re-search-forward "version \\(w3m/0\\.[3-9]\
+\\(\\.[0-9\\]\\)*\\(rc[0-9]+\\)?\
+\\(-stable\\|\\(\\+cvs\\(-[0-9]+\\.[0-9]+\\)?\\)\\)?\
 \\(-inu\\|\\(-m17n\\|\\(\\+mee\\)\\)\\)?[^,]*\\)" nil t)
 	    (setq w3m-version (match-string 1))
 	    (setq w3m-type
 		  (cond
-		   ((match-beginning 8) 'w3mmee)
-		   ((match-beginning 7) 'w3m-m17n)
+		   ((match-beginning 9) 'w3mmee)
+		   ((match-beginning 8) 'w3m-m17n)
 		   ((match-beginning 1) 'w3m)
 		   (t 'other))))
 	  (when (re-search-forward "options +" nil t)
