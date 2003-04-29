fcb0a34d019f2c33bad30aad94f1c5d6c0741a78
minakaji <>
2003-04-29 12:39:52 +0000

    New file.

---
+++ shimbun/sb-jpo.el
@@ -0,0 +1,231 @@
+;;; sb-jpo.el --- shimbun backend for http://www.jpo.go.jp -*- coding: iso-2022-7bit; -*-
+
+;; Copyright (C) 2003 NAKAJIMA Mikio <minakaji@namazu.org>
+
+;; Author: NAKAJIMA Mikio <minakaji@namazu.org>
+;; Keywords: news
+;; Created: Apr. 28, 2003
+
+;; This file is a part of shimbun.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program; if not, you can either send email to this
+;; program's maintainer or write to: The Free Software Foundation,
+;; Inc.; 59 Temple Place, Suite 330; Boston, MA 02111-1307, USA.
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'shimbun)
+
+(luna-define-class shimbun-jpo (shimbun) ())
+
+(defconst shimbun-jpo-url "http://www.jpo.go.jp/")
+(defvar shimbun-jpo-groups
+  '("news" ;■プレス発表   ■広報の広場
+    "revision" ;■法令改正のお知らせ
+    "lawguide" ;■制度の紹介
+    "details" ; ■出願から審査、審判、登録まで  ■特許庁の取り組み  ■資料室
+    ))
+(defvar shimbun-jpo-from-address "webmaster@jpo.go.jp")
+(defvar shimbun-jpo-coding-system 'japanese-shift-jis)
+(defvar shimbun-jpo-content-start "<body [^\n]+>")
+(defvar shimbun-jpo-content-end "<\/body>")
+
+;;(luna-define-method shimbun-reply-to ((shimbun shimbun-jpo))
+;;  (shimbun-from-address-internal shimbun))
+
+(luna-define-method shimbun-headers ((shimbun shimbun) &optional range)
+  (shimbun-jpo-headers shimbun))
+
+(defun shimbun-jpo-headers (shimbun)
+  (let ((group (shimbun-current-group-internal shimbun))
+	(url shimbun-jpo-url)
+	headers)
+    (with-temp-buffer
+      (if (string= group "news")
+	  (progn
+	    (setq url (concat url "torikumi/torikumi_list.htm"))
+	    (shimbun-retrieve-url url 'reload)
+	    (setq headers (shimbun-jpo-headers-1
+			   shimbun url "\\(hiroba/.*\\)"))
+	    (erase-buffer)
+	    (setq url (concat shimbun-jpo-url
+			      "torikumi/puresu/puresu_list.htm"))
+	    (setq headers (nconc headers (shimbun-jpo-headers-1 shimbun url))))
+	(if (string= group "details")
+	    (setq headers (shimbun-jpo-headers-group-details shimbun))
+	  (if (string= group "revision")
+	      (setq url (concat url "torikumi/kaisei/kaisei2/kaisei_list.htm"))
+	    (if (string= group "lawguide")
+		(setq url (concat url "seido/seido_list.htm"))
+	      (error "unknown group %s" group)))
+	  (shimbun-retrieve-url url 'reload)
+	  (setq headers (shimbun-jpo-headers-1 shimbun url)))))
+    headers))
+
+(defun shimbun-jpo-headers-1 (shimbun origurl &optional urlregexp unmatchregexp)
+  (let ((case-fold-search t)
+	(from (shimbun-from-address-internal shimbun))
+	(group (shimbun-current-group-internal shimbun))
+	(regexp (format "<td><font color=\"[#0-9A-Z]+\"><a href=\"\\(%s\\.html*\\)\">\\(.*\\)</a>[　 ]*\\([.0-9]+\\)" (or urlregexp "\\(.*\\)")))
+	(urlprefix
+	 (when (string-match "^\\(http:\/\/.+\\)\\/[^\/]+\\.html*" origurl)
+	   (match-string 1 origurl)))
+	headers id pagename subject tempdate date url)
+    ;; <td><font color="#2346AB"><a href="h1504_pat_kijitu.htm">特許法等の一部を改正する法律の一部の施行期日を定める政令案について</a>　2003.4.21</font></td>
+    ;; getting URL and SUBJECT
+    (goto-char (point-min))
+    (while (re-search-forward regexp nil t)
+      (catch 'next
+	(setq pagename (match-string-no-properties 1)
+	      subject (match-string-no-properties 3)
+	      date (match-string-no-properties 4))
+	(when (and unmatchregexp (string-match unmatchregexp pagename))
+	  (throw 'next nil))
+	(setq url (concat urlprefix "/" pagename)
+	      subject (with-temp-buffer
+			  (insert subject)
+			  (shimbun-remove-markup)
+			  (buffer-string)))
+	;; getting DATE
+	(if (not (string-match
+		  "\\([0-9]+\\)\\.\\([0-9]+\\)\\(\\.[0-9]+\\)?" date))
+	    (throw 'next nil) ; unknown date format
+	  (setq tempdate (list (string-to-number (match-string 1 date))
+			       (string-to-number (match-string 2 date))))
+	  (setq date (nconc tempdate 
+			    (list
+			     (if (not (match-string 3 date))
+				1
+			       (string-to-number
+				(substring (match-string 3 date) 1)))))))
+	;; building ID
+	(setq id (format
+		  "<%04d%02d%02d%%%s%%%s@jpo>"
+		  (car date) (nth 1 date) (nth 2 date) pagename group))
+	(setq date (apply 'shimbun-make-date-string date))
+	(unless (shimbun-search-id shimbun id)
+	  (push (shimbun-make-header
+		 0 (shimbun-mime-encode-string subject)
+		 from date id "" 0 0 url)
+		headers))
+	(forward-line 1)))
+    headers))
+
+(defun shimbun-jpo-headers-group-details (shimbun)
+  (let ((case-fold-search t)
+	(urllist '("torikumi/torikumi_list.htm"
+		   "tetuzuki/tetuzuki_list.htm"
+		   "shiryou/shiryou_list.htm"))
+	(exceptions-alist
+	 '(("torikumi/" "kaisei/" "puresu/" "hiroba/")))
+	(url shimbun-jpo-url)
+	headers pages urlprefix temp exceptions ex)
+    (while urllist
+      (when (string-match "\\/" (car urllist))
+	(setq urlprefix (substring (car urllist) 0 (1+ (match-beginning 0)))))
+      (setq url (concat shimbun-jpo-url (car urllist)))
+      (erase-buffer)
+      (shimbun-retrieve-url url 'reload)
+      (setq temp (cdr (assoc urlprefix exceptions-alist)))
+      (goto-char (point-min))
+      (setq headers (nconc headers
+			   (shimbun-jpo-headers-1
+			    shimbun url nil
+			    (when temp
+			      (concat "\\(" 
+				      (mapconcat 'regexp-quote temp "\\|")
+				      "\\)")))))
+      (goto-char (point-min))
+      (while (re-search-forward
+	      ;;<td><font color="#2346AB"><a href="puresu/puresu_list.htm">プレス発表</a></font></td>
+	      "<td><font color=\"[#0-9A-Z]+\"><a href=\"\\(.*\\.htm\\)\">[^<>]+<\/a><\/font><\/td>"
+	      nil t nil)
+	(catch 'next
+	  (setq temp (match-string 1)
+		exceptions (cdr (assoc urlprefix exceptions-alist)))
+	  (while (setq e (car exceptions))
+	    (if (string-match e temp)
+		(throw 'next nil)
+	      (setq exceptions (cdr exceptions))))
+	  (setq pages (cons (concat urlprefix temp) pages))))
+      (while pages
+	(setq url (concat shimbun-jpo-url (car pages)))
+	(erase-buffer)
+	(shimbun-retrieve-url url 'reload)
+	(setq headers (nconc
+		       headers
+		       (shimbun-jpo-headers-1 shimbun url)))
+	(setq pages (cdr pages)))
+      (setq urllist (cdr urllist)))
+    headers))
+
+(luna-define-method shimbun-make-contents ((shimbun shimbun) header)
+  (shimbun-jpo-contents shimbun header))
+
+(defun shimbun-jpo-contents (shimbun header)
+  (let ((case-fold-search t)
+	start)
+    (when (and (re-search-forward (shimbun-content-start-internal shimbun)
+				  nil t)
+	       (setq start (point))
+	       (re-search-forward (shimbun-content-end-internal shimbun)
+				  nil t))
+      (delete-region (match-beginning 0) (point-max))
+      (delete-region (point-min) start))
+    (goto-char (point-min))
+    (when (re-search-forward
+	   ;;Ｅ−mail：<a href="mailto:PA0A00@jpo.go.jp">PA0A00@jpo.go.jp<br>
+	   ;;E-mail：<a href="mailto:PA0420@jpo.go.jp">PA0420@jpo.go.jp</a></font>
+	   ;;　E-mail:<a href="mailto:PA0A00@jpo.go.jp"> PA0A00@jpo.go.jp</a></font>
+	   "[ＥｅEe][−-]*[ＭｍMm][ＡａAa][ＩｉIi][ＬｌLl][：:　] *<a href=\"mailto:\\(.*@jpo.go.jp\\)\"> *\\1"
+	   nil t nil)
+      (shimbun-header-set-from header (match-string 1)))
+    (shimbun-jpo-cleanup-article)
+    (goto-char (point-min))
+    (insert "<html>\n<head>\n<base href=\""
+	    (shimbun-header-xref header) "\">\n</head>\n<body>\n")
+    (goto-char (point-max))
+    (insert "\n</body>\n</html>\n")
+    (shimbun-make-mime-article shimbun header)
+    (buffer-string)))
+
+(defun shimbun-jpo-cleanup-article ()
+  (save-excursion
+    (let ((case-fold-search t))
+      (goto-char (point-min))
+      (when (re-search-forward 
+	     "<tr>\n+<td align=\"left\"><img src=\"\\(\\.\\./\\)?images/title\\.gif\" *[^<>]+\">\\(<\/a>\\)?<\/td>\n+<\/tr>"
+	     nil t nil)
+	(delete-region (match-beginning 0) (match-end 0)))
+      (goto-char (point-min))
+      (when (re-search-forward
+	     "<td><a href=\"http://www.adobe.co.jp/products/acrobat/readstep.html"
+	     nil t nil)
+	(delete-region (match-beginning 0) (progn (end-of-line) (point))))
+      (when (re-search-forward
+	     "<td>PDFファイルを初めてお使いになる方は、 *Adobe Acrobat Readerダウンロードページへ"
+	     nil t nil)
+	(delete-region (match-beginning 0) (progn (end-of-line) (point))))
+      (goto-char (point-min))
+      (when (re-search-forward 
+	     "<tr>\n+<td align=\"center\"><a href=\"#top\">\
+<img src=\"\\(\\.\\.\/\\)?images/gotop\\.gif\" [^<>]+\">\\(<\/a>\\)?<\/td>\n+<\/tr>"
+	     nil t nil)
+	(delete-region (match-beginning 0) (match-end 0))))))
+
+(provide 'sb-jpo)
+
+;;; sb-jpo.el ends here
