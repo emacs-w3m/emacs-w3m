9b1fd353f0b55a64b95668c2360db79c186efdbe
Katsumi Yamaoka <yamaoka@jpl.org>
2003-10-29 06:16:05 +0000

    (w3m-broken-proxy-cache): Improve the docstring.

--- w3m.el
+++ w3m.el
@@ -631,7 +631,12 @@ each time you visit a new page."
 
 (defcustom w3m-broken-proxy-cache nil
   "*Set this variable to t if the proxy server seems not to work properly
-in caching.  It may not be effective if you are using old w3m command."
+in caching.  However, this may be the double-edged sword; setting it
+to t will likely be harmful if the proxy server sends bad requests
+\(e.g. not including the Host header, see RFC2616 section 14.23) to
+foreign servers when the w3m command specifies the \"no-cache\"
+directive.  Also note that it may not be effective if you are using
+old w3m command."
   :group 'w3m
   :type 'boolean)
 
