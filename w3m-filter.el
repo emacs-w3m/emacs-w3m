d2b6903d6b3882cab377559e06b93db4b228d5a1
Masatoshi TSUCHIYA <tsuchiya@namazu.org>
2003-12-10 14:32:28 +0000

    Reconstructed completely. (w3m-filter-rules): Change its spec and its default value. (w3m-filter): Reimplemented. (w3m-filter-delete-regions, w3m-filter-find-relationships): New functions. (w3m-filter-db, w3m-filter-db-size, w3m-filter-server-regexp, w3m-filter-setup, w3m-filter-delete-region, w3m-filter-lwn.net, w3m-filter-google.com, w3m-filter-www.zdnet.co.jp): Removed.

--- w3m-filter.el
+++ w3m-filter.el
@@ -34,149 +34,76 @@
 (provide 'w3m-filter)
 (require 'w3m)
 
-
 (defcustom w3m-filter-rules
-  '(("http://www.geocities.co.jp/Technopolis/"
+  `(("\\`http://www\\.geocities\\.co\\.jp/Technopolis/"
+     w3m-filter-delete-regions
      "<DIV ALIGN=CENTER>\n<!--*/GeoGuide/*-->" "<!--*/GeoGuide/*-->\n</DIV>")
-    ("http://linux.ascii24.com/linux/"
+    ("\\`http://[a-z]+\\.hp\\.infoseek\\.co\\.jp/"
+     w3m-filter-delete-regions
+     "<!-- START_AD_Banner -->" "<!-- END_AD_Banner -->")
+    ("\\`http://linux\\.ascii24\\.com/linux/"
+     w3m-filter-delete-regions
      "<!-- DAC CHANNEL AD START -->" "<!-- DAC CHANNEL AD END -->")
-    ("http://lwn.net/" . w3m-filter-lwn.net)
-    ("http://www.google.com/search" . w3m-filter-google.com)
-    ("http://www.zdnet.co.jp/news/" . w3m-filter-www.zdnet.co.jp)
-    ("http://www.asahi.com/" . w3m-filter-asahi-shimbun))
+    ("\\`http://www\\.google\\.com/search"
+     w3m-filter-find-relationships
+     "<a href=\\([^>]+\\)><img src=/\\(intl/[^/]+/\\)nav_next.gif"
+     "<a href=\\([^>]+\\)><img src=/\\(intl/[^/]+/\\)nav_previous.gif")
+    ("\\`http://www\\.zdnet\\.co\\.jp/news/"
+     w3m-filter-find-relationships
+     ,(concat "<a href=\\(" w3m-html-string-regexp "\\)>次のページ</a>")
+     ,(concat "<a href=\\(" w3m-html-string-regexp "\\)>前のページ</a>"))
+    ("\\`http://www\\.asahi\\.com/" w3m-filter-asahi-shimbun))
   "Rules to filter advertisements on WEB sites."
   :group 'w3m
   :type '(repeat
-	  (group
-	   :indent 4 :inline t
-	   (cons
-	    :format "%v" :offset 2
-	    (string :format "URL: %v\n" :size 0)
-	    (choice
-	     :format "%[Value Menu%] %v"
-	     (list :tag "Start and End patterns"
-		   (string :size 0 :format "Start: %v\n")
-		   (string :size 0 :format "  End: %v\n"))
-	     (function :format "Filtering function:\n      Function: %v\n"
-		       :size 0
-		       :value-to-internal
-		       (lambda (widget value)
-			 (widget-sexp-value-to-internal
-			  widget (if (functionp value) value 'ignore)))))))))
-
-
-(defvar w3m-filter-db nil) ; nil means non-initialized.
-(defconst w3m-filter-db-size 1023)
-
-;; FIXME: In fact, the function which analyzes URL exactly is required.
-(defconst w3m-filter-server-regexp "^\\([-+.A-z0-9]+://[^/]+/\\)")
-
-
-(defun w3m-filter-setup ()
-  "Initialize hash database of filtering rules."
-  (unless w3m-filter-db
-    (let ((db (make-vector w3m-filter-db-size 0)))
-      (dolist (site w3m-filter-rules)
-	(let* ((url (car site))
-	       (func (cdr site))
-	       (server (when (string-match w3m-filter-server-regexp url)
-			 (match-string 1 url)))
-	       (ident (intern server db)))
-	  (unless (symbolp func)
-	    ;; Process simple filtering rules.
-	    (setq func (` (lambda (&rest args)
-			    (w3m-filter-delete-region
-			     (, (nth 1 site))
-			     (, (nth 2 site)))))))
-	  (set ident
-	       (cons (cons (concat "^" (regexp-quote url)) func)
-		     (when (boundp ident)
-		       (symbol-value ident))))))
-      (setq w3m-filter-db db))))
+	  (cons :format "%v" :indent 4
+		(regexp :format "Regexp: %v\n" :size 0)
+		(choice
+		 :tag "Filtering Rule"
+		 (list :tag "Delete regions surrounded with these patterns"
+		       (function-item :format "" w3m-filter-delete-region)
+		       (regexp :tag "Start")
+		       (regexp :tag "End"))
+		 (list :tag "Find relationships with these patterns"
+		       (function-item :format "" w3m-filter-find-relationships)
+		       (regexp :tag "Next")
+		       (regexp :tag "Previous"))
+		 (list :tag "Filter with a user defined function"
+		       function
+		       (repeat :tag "Arguments" sexp))))))
 
 ;;;###autoload
 (defun w3m-filter (url)
   "Exec filtering rule of URL to contents in this buffer."
-  (w3m-filter-setup)
-  (and (stringp url)
-       (string-match w3m-filter-server-regexp url)
-       (let ((ident (intern-soft (match-string 1 url) w3m-filter-db)))
-	 (when ident
-	   (let (functions)
-	     (dolist (site (symbol-value ident) nil)
-	       (when (string-match (car site) url)
-		 (push (cdr site) functions)))
-	     (save-match-data
-	       (run-hook-with-args-until-success 'functions url)))))))
+  (save-match-data
+    (catch 'apply-filtering-rule
+      (dolist (elem w3m-filter-rules)
+	(when (string-match (car elem) url)
+	  (throw 'apply-filtering-rule
+		 (apply (cadr elem) url (cddr elem))))))))
 
-(defun w3m-filter-delete-region (start end)
-  "Delete region from START pattern to END pattern."
+(defun w3m-filter-delete-regions (url start end)
+  "Delete regions surrounded with a START pattern and an END pattern."
   (goto-char (point-min))
-  (let (p)
-    (and (search-forward start nil t)
-	 (setq p (match-beginning 0))
-	 (search-forward end nil t)
-	 (progn
-	   (delete-region p (match-end 0))
-	   t))))
+  (let (p (i 0))
+    (while (and (search-forward start nil t)
+		(setq p (match-beginning 0))
+		(search-forward end nil t))
+      (delete-region p (match-end 0))
+      (incf i))
+    (> i 0)))
 
+(defun w3m-filter-find-relationships (url next previous)
+  "Search relationships with a NEXT pattern and a PREVIOUS pattern."
+  (goto-char (point-max))
+  (and (not w3m-next-url)
+       (re-search-backward next nil t)
+       (setq w3m-next-url (w3m-expand-url (match-string 1) url)))
+  (and (not w3m-previous-url)
+       (re-search-backward previous nil t)
+       (setq w3m-previous-url (w3m-expand-url (match-string 1) url))))
 
 ;; Filter functions:
-(defun w3m-filter-lwn.net (url)
-  "Filtering function of lwn.net."
-  (goto-char (point-min))
-  (let (p)
-    (while (and (re-search-forward "<!-- Start of Ad ([-A-z0-9]+) -->" nil t)
-		(setq p (match-beginning 0))
-		(search-forward "<!-- End of Ad -->" nil t))
-      (delete-region p (match-end 0))))
-  (goto-char (point-min))
-  (when (re-search-forward
-	 "<a href=\"\\(/[0-9][0-9][0-9][0-9]/[0-9][0-9][0-9][0-9]/[A-z]+\\.php3\\)\"><img[^>]*>Next:"
-	 nil t)
-    (let ((next (match-string 1)))
-      (goto-char (point-min))
-      (when (search-forward "<head>" nil t)
-	(insert "\n<link rel=\"next\" href=\"" next "\">"))))
-  t)
-
-(defun w3m-filter-google.com (url)
-  "Add <LINK> tag to search results of www.google.com."
-  (goto-char (point-max))
-  (let ((next (when (re-search-backward
-		     "<a href=\\([^>]+\\)><img src=/\\(intl/[^/]+/\\)nav_next.gif"
-		     nil t)
-		(match-string 1)))
-	(prev (when (re-search-backward
-		     "<a href=\\([^>]+\\)><img src=/\\(intl/[^/]+/\\)nav_previous.gif"
-		     nil t)
-		(match-string 1))))
-    (goto-char (point-min))
-    (when (search-forward "<head>" nil t)
-      (when prev (insert "\n<link rel=\"prev\" href=\"" prev "\">"))
-      (when next (insert "\n<link rel=\"next\" href=\"" next "\">")))
-    t))
-
-(defun w3m-filter-www.zdnet.co.jp (url)
-  (goto-char (point-max))
-  (let ((next (when (re-search-backward
-		     (eval-when-compile
-		       (concat
-			"<a href=\\(" w3m-html-string-regexp "\\)>次のページ</a>"))
-		     nil t)
-		(match-string 1)))
-	(prev (when (re-search-backward
-		     (eval-when-compile
-		       (concat
-			"<a href=\\(" w3m-html-string-regexp "\\)>前のページ</a>"))
-		     nil t)
-		(match-string 1))))
-    (goto-char (point-min))
-    (when (search-forward "<head>" nil t)
-      (when prev (insert "\n<link rel=\"prev\" href=" prev ">"))
-      (when next (insert "\n<link rel=\"next\" href=" next ">")))
-    t))
-
 (defun w3m-filter-asahi-shimbun (url)
   "Convert entity reference of UCS."
   (when w3m-use-mule-ucs
