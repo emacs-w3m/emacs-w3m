1f38315168bf6314010ce92012c220e909fef65e
Katsumi Yamaoka <yamaoka@jpl.org>
2003-12-12 08:29:17 +0000

    (w3m-xmas-run-at-time): Use a simple function definition if there is not  a bug in `start-itimer'.

--- w3m-xmas.el
+++ w3m-xmas.el
@@ -770,39 +770,67 @@ italic font in the modeline."
 	   (find-charset 'mule-unicode-e000-ffff)))
   (defalias 'w3m-mule-unicode-p 'ignore))
 
-(defun w3m-xmas-run-at-time (time repeat function &rest args)
-  "Emulating function run as `run-at-time' in the right way.
-TIME should be nil meaning now or a number of seconds from now.
+(if (condition-case nil
+	(progn
+	  (unless (or itimer-process itimer-timer)
+	    (itimer-driver-start))
+	  ;; Check whether there is a bug to which the difference of
+	  ;; the present time and the time when the itimer driver was
+	  ;; woken up is subtracted from the initial itimer value.
+	  (let* ((inhibit-quit t)
+		 (ctime (current-time))
+		 (itimer-timer-last-wakeup
+		  (prog1
+		      ctime
+		    (setcar ctime (1- (car ctime)))))
+		 (itimer-list nil)
+		 (itimer (start-itimer "w3m-run-at-time" 'ignore 5)))
+	    (sleep-for 0.1) ;; Accept the timeout interrupt.
+	    (prog1
+		(> (itimer-value itimer) 0)
+	      (delete-itimer itimer))))
+      (error nil))
+    (defun w3m-xmas-run-at-time (time repeat function &rest args)
+      "Emulating function run as `run-at-time'.
+TIME should be nil meaning now, or a number of seconds from now.
 Return an itimer object which can be used in either `delete-itimer'
 or `cancel-timer'."
-  (let ((itimers (list nil)))
-    (setcar
-     itimers
-     (apply #'start-itimer "w3m-run-at-time"
-	    (lambda (itimers repeat function &rest args)
-	      (let ((itimer (car itimers)))
-		(if repeat
-		    (progn
-		      (set-itimer-function
-		       itimer
-		       (lambda (itimer repeat function &rest args)
-			 (set-itimer-restart itimer repeat)
-			 (set-itimer-function itimer function)
-			 (set-itimer-function-arguments itimer args)
-			 (apply function args)))
-		      (set-itimer-function-arguments
-		       itimer
-		       (append (list itimer repeat function) args)))
-		  (set-itimer-function
-		   itimer
-		   (lambda (itimer function &rest args)
-		     (delete-itimer itimer)
-		     (apply function args)))
-		  (set-itimer-function-arguments
-		   itimer
-		   (append (list itimer function) args)))))
-	    1e-9 (if time (max time 1e-9) 1e-9)
-	    nil t itimers repeat function args))))
+      (apply #'start-itimer "w3m-run-at-time"
+	     function (if time (max time 1e-9) 1e-9)
+	     repeat nil t args))
+  (defun w3m-xmas-run-at-time (time repeat function &rest args)
+    "Emulating function run as `run-at-time' in the right way.
+TIME should be nil meaning now, or a number of seconds from now.
+Return an itimer object which can be used in either `delete-itimer'
+or `cancel-timer'."
+    (let ((itimers (list nil)))
+      (setcar
+       itimers
+       (apply #'start-itimer "w3m-run-at-time"
+	      (lambda (itimers repeat function &rest args)
+		(let ((itimer (car itimers)))
+		  (if repeat
+		      (progn
+			(set-itimer-function
+			 itimer
+			 (lambda (itimer repeat function &rest args)
+			   (set-itimer-restart itimer repeat)
+			   (set-itimer-function itimer function)
+			   (set-itimer-function-arguments itimer args)
+			   (apply function args)))
+			(set-itimer-function-arguments
+			 itimer
+			 (append (list itimer repeat function) args)))
+		    (set-itimer-function
+		     itimer
+		     (lambda (itimer function &rest args)
+		       (delete-itimer itimer)
+		       (apply function args)))
+		    (set-itimer-function-arguments
+		     itimer
+		     (append (list itimer function) args)))))
+	      1e-9 (if time (max time 1e-9) 1e-9)
+	      nil t itimers repeat function args)))))
 
 (provide 'w3m-xmas)
 
