05aec827a05b29d7d1221535891ec8d001bad2b1
Katsumi Yamaoka <yamaoka@jpl.org>
2004-01-23 11:54:42 +0000

    (w3m-goto-url): Register a url to the history even if it is failed to  retrieve.  Suggested by Fujishima-san.

--- w3m.el
+++ w3m.el
@@ -6871,7 +6871,11 @@ Cannot run two w3m processes simultaneously \
 	    (with-current-buffer w3m-current-buffer
 	      (setq w3m-current-process nil)
 	      (if (not action)
-		  (goto-char (point-min))
+		  (progn
+		    (w3m-history-push w3m-current-url
+				      (list :title (or w3m-current-title
+						       "<no-title>")))
+		    (goto-char (point-min)))
 		(if (and name (w3m-search-name-anchor name))
 		    (setf (w3m-arrived-time (w3m-url-strip-authinfo orig))
 			  (w3m-arrived-time url))
