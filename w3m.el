6e71cc42e329a98b23153fb898f7bfc0e2b2e866
Masatoshi TSUCHIYA <tsuchiya@namazu.org>
2004-01-26 07:29:38 +0000

    * w3m.el (w3m-auto-show): Check `truncate-lines' before scrolling. (w3m-check-current-position): Do not check `truncate-lines'.

--- w3m.el
+++ w3m.el
@@ -6376,7 +6376,8 @@ Otherwise, it defaults to `w3m-horizontal-shift-columns'."
 
 (defun w3m-auto-show ()
   "Scroll horizontally so that the point is visible."
-  (when (and w3m-auto-show
+  (when (and truncate-lines
+	     w3m-auto-show
 	     (not w3m-horizontal-scroll-done)
 	     (not (and (eq last-command this-command)
 		       (or (eq (point) (point-min))
@@ -6668,8 +6669,7 @@ function is designed as the hook function which is registered to
   "Run `w3m-after-cursor-move-hook' if the point gets away from the window.
 This function is designed as the hook function which is registered to
 `post-command-hook' by `w3m-buffer-setup'."
-  (when (and truncate-lines
-	     (/= (point) (car w3m-current-position)))
+  (when (/= (point) (car w3m-current-position))
     (run-hooks 'w3m-after-cursor-move-hook)))
 
 (defun w3m-buffer-setup ()
