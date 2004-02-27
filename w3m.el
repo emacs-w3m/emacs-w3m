da66328bbb2286e70075ae85e0ec98321b4a2c9f
Masatoshi TSUCHIYA <tsuchiya@namazu.org>
2004-02-27 10:13:40 +0000

    (emacs-w3m-version): Release 1.3.85.

--- w3m.el
+++ w3m.el
@@ -152,8 +152,8 @@
   (eval-when-compile
     (let ((rev "$Revision$"))
       (and (string-match "\\.\\([0-9]+\\) \\$\\'" rev)
-	   (setq rev (- (string-to-number (match-string 1 rev)) 968))
-	   (concat "1.3.80" (if (> rev 0) (format ".%d" rev) "")))))
+	   (setq rev (- (string-to-number (match-string 1 rev)) 1006))
+	   (concat "1.3.85" (if (> rev 0) (format ".%d" rev) "")))))
   "Version number of this package.")
 
 (defgroup w3m nil
