e4f4db26dba830150b6b0bfc000c36f65d140c8a
ARISAWA Akihiro <ari@mbf.sphere.ne.jp>
2004-06-21 11:51:43 +0000

    (w3m-fontify): Call `w3m-replace-symbol' before `w3m-fontify-anchors' in order not to lose text properties.

--- w3m.el
+++ w3m.el
@@ -3295,12 +3295,12 @@ If optional RESERVE-PROP is non-nil, text property is reserved."
     (w3m-fontify-bold)
     (w3m-fontify-strike-through)
     (w3m-fontify-underline)
+    (when w3m-use-symbol
+      (w3m-replace-symbol))
     (w3m-fontify-anchors)
     (when w3m-use-form
       (w3m-fontify-forms))
     (w3m-fontify-images)
-    (when w3m-use-symbol
-      (w3m-replace-symbol))
     ;; Remove other markups.
     (goto-char (point-min))
     (while (re-search-forward "</?[A-Za-z_][^>]*>" nil t)
