8c60e42564a7c0256950b55c7e28b4cbe2cc3782
Masatoshi TSUCHIYA <tsuchiya@namazu.org>
2004-07-07 07:38:55 +0000

    (emacs-w3m-version): Release 1.4.1.

--- w3m.el
+++ w3m.el
@@ -148,7 +148,7 @@
   (unless (fboundp 'char-to-int)
     (defalias 'char-to-int 'identity)))
 
-(defconst emacs-w3m-version "1.4"
+(defconst emacs-w3m-version "1.4.1"
   "Version number of this package.")
 
 (defgroup w3m nil
