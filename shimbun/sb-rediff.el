96be755c2195e7b41d3601cdf7cdde34627d9a93
Katsumi Yamaoka <yamaoka@jpl.org>
2004-11-23 22:38:57 +0000

    (shimbun-rediff-month-alist): New variable. (shimbun-article): Signal an error if the url looks malformed;  use match-string-no-properties instead of match-string. (shimbun-clear-contents): Improve regexps matching tags to be removed. (shimbun-make-contents): New function to make true Date headers. (shimbun-rss-build-message-id): Signal an error if the url string is not  what is expected;  improve the uniqueness of Message-IDs.

--- shimbun/sb-rediff.el
+++ shimbun/sb-rediff.el
@@ -6,7 +6,7 @@
 ;; Keywords: news
 ;; Created: Nov 22, 2004
 
-;; This file is not a part of shimbun.
+;; This file is a part of shimbun.
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -38,21 +38,33 @@
 (defvar shimbun-rediff-content-start "<BR></FONT>")
 (defvar shimbun-rediff-content-end "</P></FONT></TD></TR><TR><TD>")
 
+(defconst shimbun-rediff-month-alist
+  '(("January" . 1) ("February" . 2) ("March" . 3) ("April" . 4)
+    ("May" . 5) ("June" . 6) ("July" . 7) ("August" . 8)
+    ("September" . 9) ("October" . 10) ("November" . 11) ("December" . 12)))
+
 ;; Print version has less ads
 
 (luna-define-method shimbun-article :before 
   ((shimbun shimbun-rediff) header &optional outbuf)
   (let ((url (shimbun-article-url shimbun header)))
-    (when (string-match "http://www.rediff.com/rss/redirect.php\\?\
-url=http://www.rediff.com/\\(.+\\.htm\\)" url)
-      (shimbun-header-set-xref header (concat "http://in.rediff.com/cms/print.jsp?docpath=" (match-string 1 url))))))
+    (unless 
+	(string-match 
+	 "http://www.rediff.com/rss/redirect.php\\?url=http://www.rediff.com/\\(.+\\.htm\\)" 
+	 url)
+      (error (concat "Malformed URL?  " url) ))
+    (shimbun-header-set-xref 
+     header 
+     (concat 
+      "http://in.rediff.com/cms/print.jsp?docpath=" 
+      (match-string-no-properties 1 url)))))
 
 ;; Three kinds of tags to strip from the print version
 
 ;; Type 1: 
-;; <P align=center><B>
-;; <A class="" target=new href="blah"> blah blah </A>
-;; </B></P>
+;; <P align=center>
+;; <B><A class="" target=new href="blah"> blah blah </A></B>
+;; </P>
 
 ;; Type 2:
 ;;<TABLE cellSpacing=0 cellPadding=0 width=200 align=left border=0>
@@ -64,22 +76,49 @@ url=http://www.rediff.com/\\(.+\\.htm\\)" url)
 ;; <A class="" href="blah" target=new> blah blah </A>
 ;; </STRONG></LI></UL><P>
 
-
 (luna-define-method shimbun-clear-contents :before
   ((shimbun shimbun-rediff) header)
   (when (luna-call-next-method)
-    (shimbun-remove-tags "<P align=center><B>" "</B></P>")
-    (shimbun-remove-tags "<TABLE cellSpacing=0 cellPadding=0 width=200 align=left border=0>" "</TABLE></TD></TR></TABLE>"  ) 
-    (shimbun-remove-tags "<UL><LI><STRONG>" "</STRONG></LI></UL>")
-    ) )
+    (shimbun-remove-tags "<P align=center><A [^>]+>" "</A>\\(</P>\\| \\)")
+    (shimbun-remove-tags 
+     "<TABLE cellSpacing=0 cellPadding=0 width=200 align=left border=0>" 
+     "</TABLE></TD></TR></TABLE>"  ) 
+    (shimbun-remove-tags "<UL[^>]*><LI[^>]*><STRONG>" "</STRONG></LI></UL>")))
+
+;; The default header has no date string
+;; We need to parse it from the contents and set the header
+
+(luna-define-method shimbun-make-contents :before 
+  ((shimbun shimbun-rediff) header)
+  (setq case-fold-search nil)
+  (when (re-search-forward 
+	 "\\(January\\|February\\|March\\|April\\|May\\|June\
+\\|July\\|August\\|September\\|October\\|November\\|December\\)  \
+\\([0-3][0-9]\\), \\(20[0-9][0-9]\\) | \\([0-1][0-9]:[0-6][0-9]\\) IST" nil t)
+    (shimbun-header-set-date
+     header
+     (shimbun-make-date-string 
+      (string-to-number (match-string-no-properties 3))
+      (cdr (assoc (match-string-no-properties 1) shimbun-rediff-month-alist))
+      (string-to-number (match-string-no-properties 2))
+      (match-string-no-properties 4)
+      "+05:30"))
+    (goto-char (point-min))))
 
 ;; Build unique ID for the message
+
 (luna-define-method shimbun-rss-build-message-id
   ((shimbun shimbun-rediff) url date)
-  (string-match "http://www.rediff.com/rss/redirect.php\\?\
-url=http://www.rediff.com/[A-Za-z]+/\\([0-9]+\\)/\\([^/]+\\)/\\(.+\\)\\.htm" url)
-  (concat "<" (match-string 1 url) (match-string 2 url) (match-string 3 url) "@rediff.com>" )
-  )
+  (unless 
+      (string-match 
+       "http://www.rediff.com/rss/redirect.php\\?url=http://www.rediff.com/\\([A-Za-z]+\\)/\\([0-9]+\\)/\\([^/]+\\)/\\(.+\\)\\.htm" 
+       url)
+    (error (concat "Cannot find a message-id base for " url) ))
+  (format "<%s%s%s%s@rediff.com>"
+	  (match-string-no-properties 1 url)
+	  (match-string-no-properties 2 url)
+	  (match-string-no-properties 3 url)
+	  (match-string-no-properties 4 url)))
 
 (provide 'sb-rediff)
 
