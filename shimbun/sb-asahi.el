4f4f9e059114a29c4887b938b37d70edd8141995
Katsumi Yamaoka <yamaoka@jpl.org>
2005-04-04 07:05:46 +0000

    (shimbun-asahi-prepare-article): Fix previous commit.

--- shimbun/sb-asahi.el
+++ shimbun/sb-asahi.el
@@ -651,11 +651,9 @@ and tenjin, it tries to fetch the article for that day if it failed."
 	  (setq retry (1+ retry)))))
      ((string-equal group "shopping.kishi")
       (when (re-search-forward "\
-<!--[\t\n ]*記事見出し[\t\n ]*-->\\|
-<!--[\t\n ]*Start of Headline[\t\n ]*-->\\|
-<!--[\t\n ]*FJZONE START NAME[\t\n ]*=[\t\n ]*\"MIDASHI\"[\t\n ]*-->"
+<!--[\t\n ]*FJZONE END NAME[\t\n ]*=[\t\n ]*\"MIDASHI\"[\t\n ]*-->\\|\
+<!--[\t\n ]*End of Headline[\t\n ]*-->"
 			       nil t)
-	(re-search-forward "\\([\t\n ]*<[^>]+>\\)*[\t\n ]*" nil t)
 	(insert "<!-- Start of Kiji -->")))
      (t
       (when (re-search-forward
