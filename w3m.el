66947ed5c614fd408a5c4f5debc4b37b862ea075
Katsumi Yamaoka <yamaoka@jpl.org>
2005-04-08 05:54:12 +0000

    (w3m-decode-buffer): Decode `&#nnn;' entities in 128..159.

--- w3m.el
+++ w3m.el
@@ -4048,6 +4048,17 @@ for decoding when the cdr that the data specify is not available.")
 	      (w3m-find-coding-system cs)
 	      (w3m-find-coding-system
 	       (car (rassq cs w3m-compatible-encoding-alist)))))
+    ;; Decode `&#nnn;' entities in 128..159.
+    (when (rassq w3m-current-coding-system w3m-compatible-encoding-alist)
+      (goto-char (point-min))
+      (while (re-search-forward "\
+\\(&#\\(12[89]\\|1[3-5][0-9]\\)\;\\)\\|\\(&#[Xx]\\([89][0-9A-Fa-f]\\)\;\\)"
+				nil t)
+	(insert (prog1
+		    (if (match-beginning 2)
+			(string-to-number (match-string 2))
+		      (string-to-number (match-string 4) 16))
+		  (delete-region (match-beginning 0) (match-end 0))))))
     (set-buffer-multibyte t)
     (decode-coding-region (point-min) (point-max) w3m-current-coding-system)))
 
