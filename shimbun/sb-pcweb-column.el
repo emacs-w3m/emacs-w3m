88f4615e587f1f5919d3fc4b0bcdac78e6cfe9ae
Katsumi Yamaoka <yamaoka@jpl.org>
2006-02-01 11:44:06 +0000

    Update copyright year.

--- shimbun/sb-pcweb-column.el
+++ shimbun/sb-pcweb-column.el
@@ -1,6 +1,6 @@
 ;;; sb-pcweb-column.el --- shimbun backend for PC WEB COLUMN Square
 
-;; Copyright (C) 2002, 2003, 2004, 2005 OHASHI Akira <bg66@koka-in.org>
+;; Copyright (C) 2002, 2003, 2004, 2005, 2006 OHASHI Akira <bg66@koka-in.org>
 
 ;; Author: OHASHI Akira <bg66@koka-in.org>
 ;; Keywords: news
