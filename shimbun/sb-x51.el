77250e18ba686eea2241495a5167235c14275b21
Katsumi Yamaoka <yamaoka@jpl.org>
2006-02-06 00:50:20 +0000

    Update copyright year.

--- shimbun/sb-x51.el
+++ shimbun/sb-x51.el
@@ -1,6 +1,6 @@
 ;;; sb-x51.el --- shimbun backend for x51.org -*- coding: iso-2022-7bit; -*-
 
-;; Copyright (C) 2004, 2005 Tsuyoshi CHO <tsuyoshi_cho@ybb.ne.jp>
+;; Copyright (C) 2004, 2005, 2006 Tsuyoshi CHO <tsuyoshi_cho@ybb.ne.jp>
 
 ;; Author: Tsuyoshi CHO <tsuyoshi_cho@ybb.ne.jp>
 ;; Keywords: news blog
