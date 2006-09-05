f03c867526c49c65d967ec7226b45c397700e009
Katsumi Yamaoka <yamaoka@jpl.org>
2006-09-05 06:33:38 +0000

    (shimbun-sankei-group-table): Update editoria, column, and seiron groups. (shimbun-sankei-get-headers-editoria): Rewrite. (shimbun-sankei-get-headers-column): Rewrite. (shimbun-sankei-get-headers-seiron): Rewrite. (shimbun-clear-contents): Remove headlines and blockquotes from seiron articles.

--- shimbun/sb-sankei.el
+++ shimbun/sb-sankei.el
@@ -57,12 +57,9 @@
     ("book" "読書" "news/book.htm")
     ("person" "ひと" "news/person.htm")
     ("dead" "おくやみ" "news/dead.htm")
-    ("editoria" "主張" "http://ez.st37.arena.ne.jp/cgi-bin/search/namazu.cgi\
-?query=%8E%E5%92%A3&whence=0&max=20&sort=date%3Alate&idxname=edit")
-    ("column" "産経抄" "http://ez.st37.arena.ne.jp/cgi-bin/search/namazu.cgi\
-?query=%8EY%8Co%8F%B4&whence=0&max=50&sort=date%3Alate&result=normal&idxname\
-=clm")
-    ("seiron" "正論" "http://www.sankei.co.jp/special/opi/past_seiron.htm")))
+    ("editoria" "主張" "news/editoria.htm")
+    ("column" "産経抄" "news/column.htm")
+    ("seiron" "正論" "news/seiron.htm")))
 
 (defvar shimbun-sankei-x-face-alist
   '(("default" . "X-Face: L%Y'jtF~2k?#oXVB%?3t/WE),tU/UwT%tl-omu#\
@@ -156,154 +153,194 @@
       headers))))
 
 (defun shimbun-sankei-get-headers-editoria (shimbun range)
-  (let ((today (decode-time))
-	(from (concat (shimbun-server-name shimbun)
+  (let ((from (concat (shimbun-server-name shimbun)
 		      " (" (shimbun-current-group-name shimbun) ")"))
-	year month day url time headers)
-    (while (or (and today
-		    (if (and (>= (nth 2 today) 5)
-			     (< (nth 2 today) 10))
-			(prog1
-			    t
-			  (setq year (nth 5 today)
-				month (nth 4 today)
-				day (nth 3 today)
-				url (format "\
-http://www.sankei.co.jp/news/%02d%02d%02d/morning/editoria.htm"
-					    (% year 100) month day)
-				time "05:00"
-				today nil))
-		      (setq today nil)))
-	       (and (re-search-forward
-		     ;; 1:url 2:year 3:month 4:day 5:time
-		     "<a[\t\n ]+href=\"\
-\\(http://www\\.sankei\\.co\\.jp/news/\
-\\([0-9][0-9]\\)\\([01][0-9]\\)\\([0-3][0-9]\\)\
-/morning/editoria\\.htm\\)\
-\">[\t\n 　]*\\(?:\\(?:<[^>]+>\\|主張\\)[\t\n 　]*\\)\
-+([01][0-9]/[0-3][0-9][\t\n ]+\
-\\([012][0-9]:[0-5][0-9]\\))"
-				       nil t)
-		    (prog1
-			t
-		      (setq year (+ 2000 (string-to-number (match-string 2)))
-			    month (string-to-number (match-string 3))
-			    day (string-to-number (match-string 4))
-			    url (match-string 1)
-			    time (match-string 5)))))
-      (push (shimbun-create-header
-	     0
-	     (format "主張 (%d/%d)" month day)
-	     from
-	     (shimbun-make-date-string year month day time)
-	     (format "<%d%02d%02d%%editoria.sankei.co.jp>" year month day)
-	     "" 0 0
-	     url)
-	    headers))
+	(group (shimbun-current-group-internal shimbun))
+	url urls year serial month day headers)
+    (while (re-search-forward
+	    (eval-when-compile
+	      (let ((s0 "[\t\n ]*")
+		    (s1 "[\t\n ]+"))
+		(concat
+		 "<a" s1 "href=\""
+		 ;; 1. url
+		 "\\("
+		 ;; 2. year
+		 "\\(\\(?:20\\)?[0-9][0-9]\\)"
+		 "[01][0-9][0-3][0-9]/\\(?:edi"
+		 ;; 3. serial
+		 "\\([0-9]+\\)"
+		 "\\|morning/editoria\\)\\.html?\\)"
+		 "\"" s0 ">" s0 "【主張】" s0
+		 ;; 4. subject
+		 "\\([^<]+\\)"
+		 "\\(?:" s0 "<[^>]+>\\)*" s0 "(" s0
+		 ;; 5. month
+		 "\\([01]?[0-9]\\)"
+		 "/"
+		 ;; 6. day
+		 "\\([0-3]?[0-9]\\)"
+		 s1
+		 ;; 7. time
+		 "\\([012]?[0-9]:[0-5]?[0-9]\\(?:[0-5]?[0-9]\\)?\\)"
+		 s0 ")")))
+	    nil t)
+      (unless (member (setq url (match-string 1)) urls)
+	(push url urls)
+	(when (< (setq year (string-to-number (match-string 2))) 100)
+	  (setq year (+ year 2000)))
+	(setq serial (match-string 3)
+	      month (string-to-number (match-string 5))
+	      day (string-to-number (match-string 6)))
+	(push (shimbun-create-header
+	       0
+	       (match-string 4)
+	       from
+	       (shimbun-make-date-string year month day (match-string 7))
+	       (format "<%04d%02d%02d%s%%%s.%s>"
+		       year month day
+		       (if serial
+			   (concat "." serial)
+			 "")
+		       group shimbun-sankei-top-level-domain)
+	       "" 0 0
+	       (concat shimbun-sankei-url "news/" url))
+	      headers)))
     headers))
 
 (defun shimbun-sankei-get-headers-column (shimbun range)
-  (let ((today (decode-time))
-	(from (concat (shimbun-server-name shimbun)
-		      " (" (shimbun-current-group-name shimbun) ")"))
-	year month day url time headers)
-    (while (or (and today
-		    (if (and (>= (nth 2 today) 5)
-			     (< (nth 2 today) 10))
-			(prog1
-			    t
-			  (setq year (nth 5 today)
-				month (nth 4 today)
-				day (nth 3 today)
-				url (format "\
-http://www.sankei.co.jp/news/%02d%02d%02d/morning/column.htm"
-					    (% year 100) month day)
-				time "05:00"
-				today nil))
-		      (setq today nil)))
-	       (and (re-search-forward
-		     ;; 1:url 2:year 3:month 4:day 5:time
-		     "<a[\t\n ]+href=\"\
-\\(http://www\\.sankei\\.co\\.jp/news/\
-\\([0-9][0-9]\\)\\([01][0-9]\\)\\([0-3][0-9]\\)\
-/morning/column\\.htm\\)\
-\">[\t\n 　]*\\(?:\\(?:<[^>]+>\\|産経抄\\)[\t\n 　]*\\)\
-+([01][0-9]/[0-3][0-9][\t\n ]+\
-\\([012][0-9]:[0-5][0-9]\\))"
-				       nil t)
-		    (prog1
-			t
-		      (setq year (+ 2000 (string-to-number (match-string 2)))
-			    month (string-to-number (match-string 3))
-			    day (string-to-number (match-string 4))
-			    url (match-string 1)
-			    time (match-string 5)))))
+  (let* ((name (shimbun-current-group-name shimbun))
+	 (from (concat (shimbun-server-name shimbun) " (" name ")"))
+	 (group (shimbun-current-group-internal shimbun))
+	 url year serial month day headers)
+    (while (re-search-forward
+	    (eval-when-compile
+	      (let ((s0 "[\t\n ]*")
+		    (s1 "[\t\n ]+"))
+		(concat
+		 "<a" s1 "href=\""
+		 ;; 1. url
+		 "\\(\\(?:\\.\\|http://www\\.sankei\\.co\\.jp/news\\)/"
+		 ;; 2. year
+		 "\\(\\(?:20\\)?[0-9][0-9]\\)"
+		 "[01][0-9][0-3][0-9]/\\(?:col"
+		 ;; 3. serial
+		 "\\([0-9]+\\)"
+		 "\\|morning/column\\).html?\\)"
+		 "\"" s0 ">" s0 "【産経抄】\\(?:" s0 "<[^>]+>\\)*" s0 "(" s0
+		 ;; 4. month
+		 "\\([01]?[0-9]\\)"
+		 "/"
+		 ;; 5. day
+		 "\\([0-3]?[0-9]\\)"
+		 s1
+		 ;; 6. time
+		 "\\([012]?[0-9]:[0-5]?[0-9]\\(?:[0-5]?[0-9]\\)?\\)"
+		 s0 ")")))
+	    nil t)
+      (when (eq (aref (setq url (match-string 1)) 0) ?.)
+	(setq url (concat shimbun-sankei-url "news" (substring url 1))))
+      (when (< (setq year (string-to-number (match-string 2))) 100)
+	(setq year (+ year 2000)))
+      (setq serial (match-string 3)
+	    month (string-to-number (match-string 4))
+	    day (string-to-number (match-string 5)))
       (push (shimbun-create-header
 	     0
-	     (format "産経抄 (%d/%d)" month day)
+	     (format "%s (%d/%d)" name month day)
 	     from
-	     (shimbun-make-date-string year month day time)
-	     (format "<%d%02d%02d%%editoria.sankei.co.jp>" year month day)
-	     "" 0 0
-	     url)
+	     (shimbun-make-date-string year month day (match-string 6))
+	     (format "<%04d%02d%02d%s%%%s.%s>"
+		     year month day
+		     (if serial
+			 (concat "." serial)
+		       "")
+		     group shimbun-sankei-top-level-domain)
+	     "" 0 0 url)
 	    headers))
     headers))
 
 (defun shimbun-sankei-get-headers-seiron (shimbun range)
   (let ((from (concat (shimbun-server-name shimbun)
 		      " (" (shimbun-current-group-name shimbun) ")"))
-	url year month day subject hour minute headers)
+	(group (shimbun-current-group-internal shimbun))
+	url year serial month day headers)
     (while (re-search-forward
 	    (eval-when-compile
-	      (concat
-	       "<a href=\""
-	       ;; 1. url
-	       "\\(http://www\\.sankei\\.co\\.jp/news/"
-	       ;; 2. year
-	       "\\([0-9][0-9]\\)"
-	       ;; 3. month
-	       "\\([01][0-9]\\)"
-	       ;; 4. day
-	       "\\([0-3][0-9]\\)"
-	       "/morning/seiron\\.htm\\)"
-	       "\">■?【.*正論.*>】"
-	       ;; 5. subject
-	       "\\([^<]+\\)"
-	       "</a></b>　([01][0-9]/[0-3][0-9] "
-	       ;; 6. hour
-	       "\\([012][0-9]\\)"
-	       ":"
-	       ;; 7. minute
-	       "\\([0-5][0-9]\\)"))
+	      (let ((s0 "[\t\n ]*")
+		    (s1 "[\t\n ]+"))
+		(concat
+		 "<a" s1 "href=\"\\(?:\\./\\)?"
+		 ;; 1. url
+		 "\\("
+		 ;; 2. year
+		 "\\(\\(?:20\\)?[0-9][0-9]\\)"
+		 "[01][0-9][0-3][0-9]/\\(?:sir"
+		 ;; 3. serial
+		 "\\([0-9]+\\)"
+		 "\\|morning/seiron\\)\\.html?\\)"
+		 "\"" s0 ">" s0 "【正論】" s0
+		 ;; 4. subject
+		 "\\([^<]+\\)"
+		 "\\(?:" s0 "<[^>]+>\\)*" s0 "(" s0
+		 ;; 5. month
+		 "\\([01]?[0-9]\\)"
+		 "/"
+		 ;; 6. day
+		 "\\([0-3]?[0-9]\\)"
+		 s1
+		 ;; 7. time
+		 "\\([012]?[0-9]:[0-5]?[0-9]\\(?:[0-5]?[0-9]\\)?\\)"
+		 s0 ")")))
 	    nil t)
-      (setq url (match-string 1)
-	    year (+ 2000 (string-to-number (match-string 2)))
-	    month (string-to-number (match-string 3))
-	    day (string-to-number (match-string 4))
-	    subject (match-string 5)
-	    hour (match-string 6)
-	    minute (match-string 7))
+      (setq url (concat shimbun-sankei-url "news/" (match-string 1)))
+      (when (< (setq year (string-to-number (match-string 2))) 100)
+	(setq year (+ year 2000)))
+      (setq serial (match-string 3)
+	    month (string-to-number (match-string 5))
+	    day (string-to-number (match-string 6)))
       (push (shimbun-create-header
-	     0 subject from
-	     (shimbun-make-date-string year month day
-				       (concat hour ":" minute))
-	     (format "<%d%02d%02d%s%s%%seiron.sankei.co.jp>"
-		     year month day hour minute)
+	     0
+	     (match-string 4)
+	     from
+	     (shimbun-make-date-string year month day (match-string 7))
+	     (format "<%04d%02d%02d%s%%%s.%s>"
+		     year month day
+		     (if serial
+			 (concat "." serial)
+		       "")
+		     group shimbun-sankei-top-level-domain)
 	     "" 0 0 url)
 	    headers))
     headers))
 
 (luna-define-method shimbun-clear-contents :before ((shimbun shimbun-sankei)
 						    header)
-  (if (string-equal (shimbun-current-group-internal shimbun) "column")
-      (while (re-search-forward "\\([^\n>]\\)\\(　▼\\)" nil t)
-	(replace-match "\\1。\n<p>\\2"))
-    ;; Remove advertisements.
-    (shimbun-remove-tags "<!--[\t\n ]*AdSpace\\(?:.+=.+\\)+-->"
-			 "<!--[\t\n ]*/AdSpace[\t\n ]*-->")
-    ;; Remove an advertisement between photo and hombun.
-    (shimbun-remove-tags "<!--[\t\n ]*photo\\.end[\t\n ]*-->"
-			 "<!--[\t\n ]*hombun[\t\n ]*-->"))
+  (let ((group (shimbun-current-group-internal shimbun)))
+    (cond ((string-equal group "column")
+	   (while (re-search-forward "\\([^\n>]\\)\\(　▼\\)" nil t)
+	     (replace-match "\\1。\n<p>\\2")))
+	  ((string-equal group "seiron")
+	   ;; Remove headline.
+	   (when (re-search-forward
+		  (eval-when-compile
+		    (let ((s0 "[\t\n ]*"))
+		      (concat s0 "<font[^>]+>" s0 "■" s0 "</font>" s0
+			      "<\\(?:strong\\|b\\)>[^<]+</\\(?:strong\\|b\\)>"
+			      s0 "\\(?:</font>" s0 "\\)?" "<hr>" s0 "\\(?:<p>"
+			      s0 "\\)?")))
+		  nil t)
+	     (delete-region (match-beginning 0) (match-end 0)))
+	   ;; Remove blockquote.
+	   (when (re-search-forward "[\t ]*<BLOCKQUOTE>[\t ]*" nil t)
+	     (delete-region (match-beginning 0) (match-end 0))))
+	  (t
+	   ;; Remove advertisements.
+	   (shimbun-remove-tags "<!--[\t\n ]*AdSpace\\(?:.+=.+\\)+-->"
+				"<!--[\t\n ]*/AdSpace[\t\n ]*-->")
+	   ;; Remove an advertisement between photo and hombun.
+	   (shimbun-remove-tags "<!--[\t\n ]*photo\\.end[\t\n ]*-->"
+				"<!--[\t\n ]*hombun[\t\n ]*-->"))))
   (goto-char (point-min)))
 
 (provide 'sb-sankei)
