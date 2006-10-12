f92a1bdd31849ee2db98ed362d7710ee93176106
Katsumi Yamaoka <yamaoka@jpl.org>
2006-10-12 12:18:32 +0000

    (shimbun-kantei-from-address): Shinzo Abe. (shimbun-kantei-groups): Doc fix. (shimbun-kantei-x-face-alist): Renew.

--- shimbun/sb-kantei.el
+++ shimbun/sb-kantei.el
@@ -1,6 +1,6 @@
 ;;; sb-kantei.el --- shimbun backend for kantei mail magazine backnumber -*- coding: iso-2022-7bit; -*-
 
-;; Copyright (C) 2001, 2003, 2003, 2004, 2005
+;; Copyright (C) 2001, 2003, 2003, 2004, 2005, 2006
 ;; Yuuichi Teranishi <teranisi@gohome.org>
 
 ;; Author: Yuuichi Teranishi <teranisi@gohome.org>
@@ -32,10 +32,10 @@
 (luna-define-class shimbun-kantei (shimbun) ())
 
 (defvar shimbun-kantei-url "http://www.kantei.go.jp/")
-(defvar shimbun-kantei-from-address "koizumi@mmz.kantei.go.jp")
+(defvar shimbun-kantei-from-address "Shinzo Abe")
 
 (defvar shimbun-kantei-groups '("m-magazine-en" "m-magazine-ja" "m-magazine")
-  "List of groups of the Koizumi cabinet email magazine.
+  "List of groups of the Abe cabinet email magazine.
 Note that the `m-magazine-ja' is the same as `m-magazine' which is for
 the backward compatibility.")
 
@@ -44,10 +44,11 @@ the backward compatibility.")
 (defvar shimbun-kantei-content-end
   "\\(<!-- /CONTENT -->\\)\\|\\(</PRE>\\)\n</FONT>\n</TD></TR></TABLE>")
 (defvar shimbun-kantei-x-face-alist
-  '(("default" . "X-Face: .bsmj'!8A`wI\\o+KF!)#0.a0,f1MA~PH/5T0\
-fu$Mg+)_5G~NSk4.0t]&|f@^c3l8-Fuz8'|\n kr;td_Jn7|GwREbDs'H9$Iy#y\
-M#*J2c'L},(m8K:8?$vTPC%D}YJ[bV#7xw|{\"DJ:_?`V1m_4^+;7+\n JOf6v&\
-x6?mU-q=0}mTK5@\"-bFGuD}2Y/(lR/V#'?HRc2Jh2UrR,oIR~NL!})|^%kw")))
+  '(("default" . "X-Face: #j64iK&$Ao220A!\\\\tqgtA2?%O+6agSQYwasO-bR=\
+i4jvi)sY%u]:><N>|r$nM\\pO+K.R<<\n VbLue|jYf&\"2t)<UZ.TF!BW}d>!;<T]KH'\
+Wnxf\\Yz}-]SF#S~=VQf/X{h5\\$UmliCX7wmhs,&L&7I.\n Jr?~k2:Xf}go(QDd=k?W\
++:^tpFH['bX(8qw&5|h?Mte<:qn;E0[\\{|FP]e*GyxX0[CrOd14_:@o7rI\n T3oI(>,\
+o<MT~H(]>J5C\":P37R[`|,G2f-7!2q2q}IYksyDF8YvSMy~CpjBmhL!V@,f@@ak;{2ATB")))
 
 (luna-define-method shimbun-index-url ((shimbun shimbun-kantei))
   (let ((group (shimbun-current-group-internal shimbun)))
