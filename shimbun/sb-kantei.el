c9da71d0e6c02add7ee7bdd5b9947cfaf6dd4241
Katsumi Yamaoka <yamaoka@jpl.org>
2006-10-13 08:16:53 +0000

    (shimbun-kantei-from-address): Remove. (shimbun-kantei-groups): Add m-magazine-ja.koizumi group. (shimbun-kantei-x-face-alist): Restore Koizumi's face. (shimbun-index-url): Work with m-magazine-ja.koizumi group;  make m-magazine group an alias to m-magazine-ja.koizumi group. (shimbun-from-address): Return a correct author. (shimbun-get-headers): Update regexp for English articles;  remove too match whitespace in Japanese subject. (shimbun-clear-contents): Remove <CENTER> tags in English article.

--- shimbun/sb-kantei.el
+++ shimbun/sb-kantei.el
@@ -32,43 +32,58 @@
 (luna-define-class shimbun-kantei (shimbun) ())
 
 (defvar shimbun-kantei-url "http://www.kantei.go.jp/")
-(defvar shimbun-kantei-from-address "Shinzo Abe")
 
-(defvar shimbun-kantei-groups '("m-magazine-en" "m-magazine-ja" "m-magazine")
-  "List of groups of the Abe cabinet email magazine.
-Note that the `m-magazine-ja' is the same as `m-magazine' which is for
-the backward compatibility.")
+(defvar shimbun-kantei-groups '("m-magazine-en" "m-magazine-ja"
+				;;"m-magazine-en.koizumi"
+				"m-magazine-ja.koizumi"
+				"m-magazine")
+  "List of the groups subscribing to the email magazin of Japan's Cabinet.
+Note that the `m-magazine-ja.koizumi' is the same as `m-magazine' which
+is for the backward compatibility.")
 
 (defvar shimbun-kantei-content-start
   "<!-- CONTENT -->\\|<PRE>")
+
 (defvar shimbun-kantei-content-end
   "\\(<!-- /CONTENT -->\\)\\|\\(</PRE>\\)\n</FONT>\n</TD></TR></TABLE>")
+
 (defvar shimbun-kantei-x-face-alist
   '(("default" . "X-Face: #j64iK&$Ao220A!\\\\tqgtA2?%O+6agSQYwasO-bR=\
 i4jvi)sY%u]:><N>|r$nM\\pO+K.R<<\n VbLue|jYf&\"2t)<UZ.TF!BW}d>!;<T]KH'\
 Wnxf\\Yz}-]SF#S~=VQf/X{h5\\$UmliCX7wmhs,&L&7I.\n Jr?~k2:Xf}go(QDd=k?W\
 +:^tpFH['bX(8qw&5|h?Mte<:qn;E0[\\{|FP]e*GyxX0[CrOd14_:@o7rI\n T3oI(>,\
-o<MT~H(]>J5C\":P37R[`|,G2f-7!2q2q}IYksyDF8YvSMy~CpjBmhL!V@,f@@ak;{2ATB")))
+o<MT~H(]>J5C\":P37R[`|,G2f-7!2q2q}IYksyDF8YvSMy~CpjBmhL!V@,f@@ak;{2ATB")
+    ("\\.koizumi\\'\\|\\`m-magazine\\'" . "X-Face: .bsmj'!8A`wI\\o+KF\
+!)#0.a0,f1MA~PH/5T0fu$Mg+)_5G~NSk4.0t]&|f@^c3l8-Fuz8'|\n kr;td_Jn7|Gw\
+REbDs'H9$Iy#yM#*J2c'L},(m8K:8?$vTPC%D}YJ[bV#7xw|{\"DJ:_?`V1m_4^+;7+\n\
+ JOf6v&x6?mU-q=0}mTK5@\"-bFGuD}2Y/(lR/V#'?HRc2Jh2UrR,oIR~NL!})|^%kw")))
 
 (luna-define-method shimbun-index-url ((shimbun shimbun-kantei))
   (let ((group (shimbun-current-group-internal shimbun)))
     (concat (shimbun-url-internal shimbun)
 	    (cond ((string-equal group "m-magazine-en")
-		   "foreign/m-magazine")
+		   "foreign/m-magazine/backnumber/")
+		  ;;((string-equal group "m-magazine-en.koizumi")
+		  ;; "???")
+		  ((string-equal group "m-magazine-ja.koizumi")
+		   "jp/m-magazine/backnumber/koizumi.html")
 		  ((string-equal group "m-magazine") ;; Backward compatibility.
-		   "jp/m-magazine")
+		   "jp/m-magazine/backnumber/koizumi.html")
 		  (t
-		   "jp/m-magazine"))
-	    "/backnumber/")))
+		   "jp/m-magazine/backnumber/")))))
 
 (luna-define-method shimbun-from-address ((shimbun shimbun-kantei))
-  (format "%s <%s>"
-	  (shimbun-mime-encode-string
-	   (if (string-equal (shimbun-current-group-internal shimbun)
-			     "m-magazine-en")
-	       "Official Residence"
-	     "首相官邸"))
-	  (shimbun-from-address-internal shimbun)))
+  (let ((group (shimbun-current-group-internal shimbun)))
+    (cond ((string-equal group "m-magazine-en")
+	   "Shinzo Abe")
+	  ;;((string-equal group "m-magazine-en.koizumi")
+	  ;; "Junichiro Koizumi")
+	  ((string-equal group "m-magazine-ja.koizumi")
+	   (shimbun-mime-encode-string "小泉純一郎"))
+	  ((string-equal group "m-magazine") ;; Backward compatibility.
+	   (shimbun-mime-encode-string "小泉純一郎"))
+	  (t
+	   (shimbun-mime-encode-string "阿部晋三")))))
 
 (luna-define-method shimbun-get-headers ((shimbun shimbun-kantei)
 					 &optional range)
@@ -78,28 +93,23 @@ o<MT~H(]>J5C\":P37R[`|,G2f-7!2q2q}IYksyDF8YvSMy~CpjBmhL!V@,f@@ak;{2ATB")))
 ;;(defun shimbun-kantei-get-headers (shimbun range)
 ;;;</DEBUG>
   (let* ((group (shimbun-current-group-internal shimbun))
-	 (enp (equal group "m-magazine-en"))
+	 (enp (member group '("m-magazine-en")))
 	 (regexp (if enp
 		     (eval-when-compile
-		       (concat "\\["
-			       ;; 1. month
-			       "\\([01][0-9]\\)"
-			       "/"
-			       ;; 2. day of month
-			       "\\([0-3][0-9]\\)"
-			       "/"
-			       ;; 3. year
+		       (concat "<A[\t\n ]+HREF=\""
+			       ;; 1. url
+			       "\\("
+			       ;; 2. year
 			       "\\(20[0-9][0-9]\\)"
-			       "\\][\t\n ]*"
-			       ;; 4. serial number
-			       "\\(No\\. [0-9]+:\\)?"
-			       "\\(?:[\t\n ]*<[^A][^>]+>\\)+[\t\n ]*"
-			       "<A[\t\n ]+HREF=\""
-			       ;; 5. url
-			       "\\([^\"]+\\)"
-			       "\"[^>]*>[\t\n ]*"
-			       ;; 6. subject
-			       "\\([^<]+\\)"))
+			       "/"
+			       ;; 3. month
+			       "\\([01][0-9]\\)"
+			       ;; 4. day of month
+			       "\\([0-3][0-9]\\)"
+			       "\\.html\\)\">[\t\n ]*"
+			       ;; 5. subject
+			       "\\(\\(?:[^\t\n <]+[\t\n ]+\\)*[^\t\n <]+\\)"
+			       "[\t\n ]*</A>[\t\n ]*</TD>[\t\n ]*</TR>"))
 		   (eval-when-compile
 		     (concat "<A[\t\n ]+HREF=\""
 			     ;; 1. url
@@ -122,35 +132,20 @@ o<MT~H(]>J5C\":P37R[`|,G2f-7!2q2q}IYksyDF8YvSMy~CpjBmhL!V@,f@@ak;{2ATB")))
 	 year month mday url subject id headers)
     (while (re-search-forward regexp nil t)
       (if enp
-	  (progn
-	    (setq year (match-string 3)
-		  month (match-string 1)
-		  mday (match-string 2)
-		  url (match-string 5)
-		  subject (if (match-beginning 4)
-			      (concat (match-string 4) " " (match-string 6))
-			    (match-string 6)))
-	    (if (string-match "\
-\\`\\(20[0-9][0-9]\\)/\\(\\([01][0-9]\\)\\([0-3][0-9]\\)[^.]*\\)\\.html\\'"
-			      url)
-		(setq year (string-to-number (match-string 1 url))
-		      month (string-to-number (match-string 3 url))
-		      mday (string-to-number (match-string 4 url))
-		      id (format "<%d%s%s@www.kantei.or.jp>"
-				 year (match-string 2 url) group))
-	      (setq year (string-to-number year)
-		    month (string-to-number month)
-		    mday (string-to-number mday)
-		    id (format "<%d%02d%02d%s@www.kantei.or.jp>"
-			       year month mday group)))
-	    (when (string-match "[\t ]+\\'" subject)
-	      (setq subject (substring subject 0 (match-beginning 0)))))
+	  (setq year (string-to-number (match-string 2))
+		month (string-to-number (match-string 3))
+		mday (string-to-number (match-string 4))
+		url (match-string 1)
+		subject (match-string 5)
+		id (format "<%d%02d%02d.%s%%kantei.go.jp>"
+			   year month mday group))
 	(setq year (string-to-number (match-string 2))
 	      month (string-to-number (match-string 3))
 	      mday (string-to-number (match-string 4))
 	      url (match-string 1)
-	      subject (match-string 6)
-	      id (format "<%d%02d%02d%s%s@www.kantei.or.jp>"
+	      subject (shimbun-replace-in-string (match-string 6)
+						 "[\t\n 　]+" " ")
+	      id (format "<%d%02d%02d%s.%s%%kantei.go.jp>"
 			 year month mday
 			 (or (match-string 5) "")
 			 group)))
@@ -165,13 +160,18 @@ o<MT~H(]>J5C\":P37R[`|,G2f-7!2q2q}IYksyDF8YvSMy~CpjBmhL!V@,f@@ak;{2ATB")))
     headers))
 
 (luna-define-method shimbun-clear-contents ((shimbun shimbun-kantei) header)
-  (let ((case-fold-search t) start)
+  (let ((case-fold-search t)
+	start)
     (when (and (re-search-forward (shimbun-content-start shimbun) nil t)
 	       (setq start (match-beginning 0))
 	       (re-search-forward (shimbun-content-end shimbun) nil t))
       (delete-region (or (match-end 1) (match-end 2)) (point-max))
       (delete-region (point-min) start)
-      t)))
+      (when (member (shimbun-current-group-internal shimbun)
+		    '("m-magazine-en"))
+	(goto-char (point-min))
+	(while (re-search-forward "</?center>" nil t)
+	  (delete-region (match-beginning 0) (match-end 0)))))))
 
 (provide 'sb-kantei)
 
