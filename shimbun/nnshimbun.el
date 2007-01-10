eb09ceba34e0ac5b16fd2d35f12a62fc58cf1552
Katsumi Yamaoka <yamaoka@jpl.org>
2007-01-10 23:06:11 +0000

    Update copyright years.

--- shimbun/nnshimbun.el
+++ shimbun/nnshimbun.el
@@ -1,6 +1,6 @@
 ;;; nnshimbun.el --- interfacing with web newspapers
 
-;; Copyright (C) 2000, 2001, 2002, 2003, 2004, 2005, 2006
+;; Copyright (C) 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007
 ;; TSUCHIYA Masatoshi <tsuchiya@namazu.org>
 
 ;; Authors: TSUCHIYA Masatoshi <tsuchiya@namazu.org>,
