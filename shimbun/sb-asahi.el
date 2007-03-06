d4d13b1a3cf86c7dafc37e9a0ada8f5825d4ed7e
Katsumi Yamaoka <yamaoka@jpl.org>
2007-03-06 02:21:40 +0000

    Fix previous commit.

--- shimbun/sb-asahi.el
+++ shimbun/sb-asahi.el
@@ -1250,7 +1250,7 @@ and tenjin, it tries to fetch the article for that day if it failed."
 		(insert "<!-- Start of Kiji -->")
 		(when index
 		  (insert "\
-\n<p>(指定された&nbsp;url&nbspが&nbspまだ/すでに&nbsp無いので、\
+\n<p>(指定された&nbsp;url&nbsp;が&nbsp;まだ/すでに&nbsp;無いので、\
 <a href=\"" index "\">トップページ</a> から記事を取得しました)</p>\n"))
 		(while (re-search-forward "[\t\n ]*<SPAN STYLE=[^>]+>[\t\n ]*"
 					  nil t)
