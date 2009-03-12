4c1a5eda426459700321f935f841b6f9aa2a6b9b
Hideyuki SHIRAI <shirai@meadowy.org>
2009-03-12 00:40:59 +0000

    (w3m-w3m-parse-header):  The value of w3m-document-charset is not used excluding x-moe-internal.

--- w3m.el
+++ w3m.el
@@ -5054,11 +5054,7 @@ Return a list which includes:
 	    (if (string-match "\\`ftps?:.*/\\'" url)
 		(if w3m-accept-japanese-characters
 		    "w3m-euc-japan" "w3m-iso-latin-1")
-	      (or charset
-		  (and (eq w3m-type 'w3mmee)
-		       (setq charset
-			     (cdr (assoc "w3m-document-charset" headers)))
-		       (car (split-string charset)))))
+	      charset)
 	    (let ((v (cdr (assoc "content-length" headers))))
 	      (and v (setq v (string-to-number v)) (> v 0) v))
 	    (cdr (or (assoc "content-encoding" headers)
