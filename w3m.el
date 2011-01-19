bdad4e634f4ba974cfbdde919553e21cb4c410f9
Katsumi Yamaoka <yamaoka@jpl.org>
2011-01-19 01:39:05 +0000

    Update copyright years.

--- w3m.el
+++ w3m.el
@@ -1,7 +1,7 @@
 ;;; w3m.el --- an Emacs interface to w3m -*- coding: iso-2022-7bit; -*-
 
 ;; Copyright (C) 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009,
-;; 2010 TSUCHIYA Masatoshi <tsuchiya@namazu.org>
+;; 2010, 2011 TSUCHIYA Masatoshi <tsuchiya@namazu.org>
 
 ;; Authors: TSUCHIYA Masatoshi <tsuchiya@namazu.org>,
 ;;          Shun-ichi GOTO     <gotoh@taiyo.co.jp>,
