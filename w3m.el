e39556d9a0cfa90e6a606797a892fa0faed33cfa
Katsumi Yamaoka <yamaoka@jpl.org>
2015-05-08 05:43:42 +0000

    w3m.el (w3m-refresh-at-time): Temporary fix to avoid Google search looping [emacs-w3m:12440]

--- w3m.el
+++ w3m.el
@@ -9877,7 +9877,10 @@ See `w3m-default-directory'."
 (defun w3m-refresh-at-time ()
   (when (and w3m-use-refresh w3m-current-refresh)
     (if (= (car w3m-current-refresh) 0)
-	(w3m-goto-url-with-timer (cdr w3m-current-refresh) (current-buffer))
+	;; Temporary fix to avoid Google search looping [emacs-w3m:12440].
+	(unless (string-match "\\`https?://\\(?:[^\t\n /]+\\.\\)*google\\."
+			      w3m-current-url)
+	  (w3m-goto-url-with-timer (cdr w3m-current-refresh) (current-buffer)))
       (setq w3m-refresh-timer
 	    (run-at-time (car w3m-current-refresh)
 			 nil
