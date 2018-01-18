2f880fcadc94a0b775abe33416f880e13cd8d81c
Katsumi Yamaoka <yamaoka@jpl.org>
2018-01-18 04:00:23 +0000

    Add a user-agent changer by Boruch Baum
    * w3m.el (w3m-user-agent-change): New feature.
    (w3m-reload-this-page): Integrate new feature.
    (w3m-request-arguments, w3m-header-arguments): Ensure w3m and
    w3mmee honor `w3m-add-user-agent'.

--- w3m.el
+++ w3m.el
@@ -319,6 +319,23 @@ The value of `w3m-user-agent' is used for the field body."
   :group 'w3m
   :type 'boolean)
 
+(defvar w3m-user-agent-default-alist (list
+  (cons "Emacs-w3m (user default)" w3m-user-agent)
+  (cons "Emacs-w3m (package default)" (concat "Emacs-w3m/" emacs-w3m-version " " w3m-version))
+  '("Don't send user agent" . ""))
+"An default alist of user agent strings, used when offering the user the opportunity to change user agent strings. This should normally not be modified; instead modify `w3m-user-agent-alist'.")
+
+(defcustom w3m-user-agent-alist '(
+  ("Android Webkit" . "Mozilla/5.0 (Linux; U; Android 4.0.3; ko-kr; LG-L160L Build/IML74K) AppleWebkit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30")
+  ("Firefox 57" . "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:57.0) Gecko/20100101 Firefox/57.0")
+  ("IE Mobile" . "Mozilla/5.0 (compatible; MSIE 9.0; Windows Phone OS 7.5; Trident/5.0; IEMobile/9.0)")
+  ("Opera Mini 9.80" . "Opera/9.80 (J2ME/MIDP; Opera Mini/9.80 (S60; SymbOS; Opera Mobi/23.348; U; en) Presto/2.5.25 Version/10.54")
+  ("Opera Mobile 12" . "Opera/12.02 (Android 4.1; Linux; Opera Mobi/ADR-1111101157; U; en-US) Presto/2.9.201 Version/12.02")
+  ("Chrome 51" . "Mozilla/5.0 (Linux; Android 5.1.1; VS810PP Build/LMY47V) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.81 Mobile Safari/537.36"))
+"An alist of user agent strings. Each entry should be a cons of a short descriptive string and the user agent string."
+:group 'w3m
+:type '(repeat (cons (string :tag "Short Description") (string :tag "User Agent string"))))
+
 (defcustom w3m-language
   (if (and (boundp 'current-language-environment)
 	   ;; In XEmacs 21.5 it may be the one like "Japanese (UTF-8)".
@@ -5527,8 +5544,8 @@ Third optional CONTENT-TYPE is the Content-Type: field content."
       (if (and (null cookie)(null body)
 	       (null content-type))
 	  (append
-	   (when w3m-add-user-agent
-	     (list "-header" (concat "User-Agent:" w3m-user-agent)))
+           (list "-header" (concat "User-Agent:"
+             (if w3m-add-user-agent w3m-user-agent "?")))
 	   (when (w3m-add-referer-p url referer)
 	     (list "-header" (concat "Referer: " referer)))
 	   (when w3m-accept-languages
@@ -5536,7 +5553,7 @@ Third optional CONTENT-TYPE is the Content-Type: field content."
 			      "Accept-Language: "
 			      (mapconcat 'identity w3m-accept-languages
 					 " ")))))
-	(when w3m-add-user-agent (insert "User-Agent: " w3m-user-agent "\n"))
+	(insert "User-Agent: " (if w3m-add-user-agent w3m-user-agent "?") "\n")
 	(when (w3m-add-referer-p url referer)
 	  (insert "Referer: " referer "\n"))
 	(when w3m-accept-languages
@@ -5568,9 +5585,9 @@ Third optional CONTENT-TYPE is the Content-Type: field content."
   (let ((modes (default-file-modes))
 	(cookie (and w3m-use-cookies (w3m-cookie-get url)))
 	args)
-    (when w3m-add-user-agent
-      (setq args (nconc args
-			(list "-o" (concat "user_agent=" w3m-user-agent)))))
+    (setq args (nconc args
+      (list "-o" (concat "user_agent="
+        (if w3m-add-user-agent w3m-user-agent "?")))))
     (when cookie
       (setq args (nconc args
 			(list "-header" (concat "Cookie: " cookie)))))
@@ -9996,16 +10013,56 @@ buffer will start afresh."
   (interactive)
   (w3m-goto-url-new-session "about:blank"))
 
+(defun w3m-user-agent-change (&optional ua-string)
+"Return a user-agent string.
+
+Prompt the user to select from entries in
+`w3m-user-agent-default-alist', `w3m-user-agent-alist', or the
+user may manually enter a custom user-agent string.
+
+When called interactively, variables `w3m-user-agent' and
+`w3m-add-user-agent' are updated, ie. the changes are permanent."
+  (interactive "P")
+  (when (not ua-string)
+    (let ((ua-list (append w3m-user-agent-default-alist w3m-user-agent-alist)))
+     (setq ua-string
+       (or (cdr (assoc (setq ua-string
+                         (completing-read "Select a user-agent: " ua-list))
+                   ua-list))
+           ua-string))))
+  (when (string-equal ua-string "")
+    (setq ua-string nil))
+  (when (called-interactively-p 'interactive)
+    (if (not ua-string)
+      (setq w3m-add-user-agent nil)
+     (setq w3m-add-user-agent t)
+     (setq w3m-user-agent ua-string)))
+  ua-string)
+
 (defun w3m-reload-this-page (&optional arg no-popup)
   "Reload the current page, disregarding the cached contents.
-If the prefix arg ARG is given, it also clears forms and post data."
+If the prefix arg ARG is given, it also clears forms and post data.
+
+If the prefix arg is given twice, prompt the user to change the
+user-agent string to be sent for the reload.
+
+If the prefix arg is given three times, do both, ie. clear forms
+and post data, AND prompt the user to change the user-agent
+string to be sent for the reload."
   (interactive "P")
   (if w3m-current-url
-      (let ((w3m-history-reuse-history-elements
-	     ;; Don't move the history position.
-	     'reload)
-	    post-data)
-	(if arg
+    (let*
+      ((w3m-history-reuse-history-elements 'reload) ;; Don't move the history position.
+       (w3m-user-agent
+         (if (or (equal arg '(16)) (equal arg '(64)))
+           (w3m-user-agent-change)
+          w3m-user-agent))
+       (w3m-add-user-agent
+         (if (or (equal arg '(16)) (equal arg '(64)))
+           w3m-user-agent
+          w3m-add-user-agent))
+       post-data)
+	(if (or (equal arg '(4)) (equal arg '(64)))
 	    (progn
 	      (w3m-history-remove-properties '(:forms nil :post-data nil))
 	      (setq w3m-current-forms nil))
