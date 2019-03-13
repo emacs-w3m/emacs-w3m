34c9bf2100837ec067d25d9f94b2ac0077feb12e
Boruch Baum <boruch_baum@gmx.com>
2019-03-13 19:26:14 -0400

    w3m-external-view should use legacy download
    + The way external-view seems to be currently handled is that it is
      different from download, in that it is a temporary action, in the
      sense that after the downloaded file is sent to the viewer program,
      it is deleted from disk. The user can save the file from the
      external viewer, if that external program has that option.
    
    + external-view probably doesn't need a progress buffer, although it
      technically wouldn't hurt.
    
    + It does need to wait for the download to complete in order to
      proceed to execute the view command for the downloaded file. This
      could potentially be added to the wget code, using the 'handler'
      argument and the existing 'w3m-process' architecture.

--- w3m.el
+++ w3m.el
@@ -7361,7 +7361,7 @@ command instead."
 		  (error "\
 No method to view `%s' is registered. Use `w3m-edit-this-url'"
 			 (file-name-nondirectory (w3m-url-to-file-name url)))
-		(w3m-download url nil no-cache handler)))
+		(w3m-download-using-w3m url nil no-cache handler)))
 	     ((functionp method)
 	      (funcall method url))
 	     ((consp method)
@@ -7381,13 +7381,13 @@ No method to view `%s' is registered. Use `w3m-edit-this-url'"
 		 ((and command (memq 'file arguments))
 		  (let ((w3m-current-buffer (current-buffer)))
 		    (w3m-process-do
-			(success (w3m-download url file no-cache handler))
+			(success (w3m-download-using-w3m url file no-cache handler))
 		      (when success
 			(w3m-external-view-file command file url arguments)))))
 		 (command
 		  (w3m-external-view-file command nil url arguments))
 		 (t
-		  (w3m-download url nil no-cache handler))))))))))))
+		  (w3m-download-using-w3m url nil no-cache handler))))))))))))
 
 (defun w3m-external-view-file (command file url arguments)
   ;; The 3rd argument `url' is necessary to handle the constant `url'
